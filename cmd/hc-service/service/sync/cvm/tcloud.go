1828924cd692e75ca2035692801b9e1314e2380f
guohuliu <guohuliu@tencent.com>
2023-03-06 22:05:02 +0800

    fix: 修复腾讯云全量同步没有删除db不存在CVM数据的问题 (merge request !264)
    Squash merge branch 'tcloud-cvm-fix' into 'v1.0'
    fix: 修复腾讯云全量同步没有删除db不存在CVM数据的问题

--- cmd/hc-service/service/sync/cvm/tcloud.go
+++ cmd/hc-service/service/sync/cvm/tcloud.go
@@ -21,12 +21,19 @@ package cvm
 
 import (
 	"hcm/cmd/hc-service/logics/sync/cvm"
+	"hcm/pkg/adaptor/tcloud"
 	typecore "hcm/pkg/adaptor/types/core"
 	typecvm "hcm/pkg/adaptor/types/cvm"
+	"hcm/pkg/api/core"
+	dataproto "hcm/pkg/api/data-service/cloud"
 	"hcm/pkg/api/hc-service/sync"
 	"hcm/pkg/criteria/errf"
+	"hcm/pkg/dal/dao/tools"
+	"hcm/pkg/kit"
 	"hcm/pkg/logs"
 	"hcm/pkg/rest"
+	"hcm/pkg/runtime/filter"
+	"hcm/pkg/tools/converter"
 )
 
 // SyncTCloudCvm ...
@@ -45,6 +52,7 @@ func (svc *syncCvmSvc) SyncTCloudCvm(cts *rest.Contexts) (interface{}, error) {
 		return nil, err
 	}
 
+	cloudCvmTotalMap := make(map[string]struct{}, 0)
 	listOpt := &typecvm.TCloudListOption{
 		Region:   req.Region,
 		CloudIDs: nil,
@@ -66,6 +74,7 @@ func (svc *syncCvmSvc) SyncTCloudCvm(cts *rest.Contexts) (interface{}, error) {
 
 		cloudIDs := make([]string, 0, len(cvms))
 		for _, one := range cvms {
+			cloudCvmTotalMap[*one.InstanceId] = struct{}{}
 			cloudIDs = append(cloudIDs, *one.InstanceId)
 		}
 
@@ -86,7 +95,102 @@ func (svc *syncCvmSvc) SyncTCloudCvm(cts *rest.Contexts) (interface{}, error) {
 		listOpt.Page.Offset += typecore.TCloudQueryLimit
 	}
 
+	if err = svc.removeDBNotExistCvm(cts.Kit, cli, req, cloudCvmTotalMap); err != nil {
+		logs.Errorf("remove db not exist cvm failed, err: %v, rid: %s", err, cts.Kit.Rid)
+		return nil, err
+	}
+
 	return nil, nil
+}
+
+func (svc *syncCvmSvc) removeDBNotExistCvm(kt *kit.Kit, tcloud *tcloud.TCloud, req *sync.TCloudSyncReq,
+	cloudCvmMap map[string]struct{}) error {
+
+	// 找出云上已经不存在的主机ID
+	start := uint32(0)
+	delCloudCvmIDs := make([]string, 0)
+	for {
+		listReq := &dataproto.CvmListReq{
+			Field: []string{"cloud_id"},
+			Filter: &filter.Expression{
+				Op: filter.And,
+				Rules: []filter.RuleFactory{
+					&filter.AtomRule{Field: "account_id", Op: filter.Equal.Factory(), Value: req.AccountID},
+					&filter.AtomRule{Field: "region", Op: filter.Equal.Factory(), Value: req.Region},
+				},
+			},
+			Page: &core.BasePage{
+				Start: start,
+				Limit: core.DefaultMaxPageLimit,
+			},
+		}
+		result, err := svc.dataCli.Global.Cvm.ListCvm(kt.Ctx, kt.Header(), listReq)
+		if err != nil {
+			logs.Errorf("list cvm from db failed, err: %v, rid: %s", err, kt.Rid)
+			return err
+		}
+
+		for _, one := range result.Details {
+			if _, exist := cloudCvmMap[one.CloudID]; !exist {
+				delCloudCvmIDs = append(delCloudCvmIDs, one.CloudID)
+			}
+		}
+
+		if len(result.Details) < int(core.DefaultMaxPageLimit) {
+			break
+		}
+
+		start += uint32(core.DefaultMaxPageLimit)
+	}
+
+	// 再用这部分主机ID去云上确认是否存在，如果不存在，删除db数据，存在的忽略，等同步修复
+	start, end := 0, typecore.TCloudQueryLimit
+	for {
+		if int(start)+end > len(delCloudCvmIDs) {
+			end = len(delCloudCvmIDs)
+		} else {
+			end = int(start) + typecore.TCloudQueryLimit
+		}
+		tmpCloudIDs := delCloudCvmIDs[start:end]
+
+		if len(tmpCloudIDs) == 0 {
+			break
+		}
+
+		listOpt := &typecvm.TCloudListOption{
+			Region:   req.Region,
+			CloudIDs: tmpCloudIDs,
+		}
+		cvms, err := tcloud.ListCvm(kt, listOpt)
+		if err != nil {
+			logs.Errorf("list cvm from tcloud failed, err: %v, opt: %v, rid: %s", err, listOpt, kt.Rid)
+			return err
+		}
+
+		tmpMap := converter.StringSliceToMap(tmpCloudIDs)
+		for _, instance := range cvms {
+			delete(tmpMap, *instance.InstanceId)
+		}
+
+		if len(tmpMap) == 0 {
+			start = uint32(end)
+			continue
+		}
+
+		if err = svc.dataCli.Global.Cvm.BatchDeleteCvm(kt.Ctx, kt.Header(), &dataproto.CvmBatchDeleteReq{
+			Filter: tools.ContainersExpression("cloud_id", converter.MapKeyToStringSlice(tmpMap)),
+		}); err != nil {
+			logs.Errorf("batch delete db cvm failed, err: %v, rid: %s", err, kt.Rid)
+			return err
+		}
+
+		start = uint32(end)
+		if start == uint32(len(delCloudCvmIDs)) {
+			break
+		}
+	}
+
+	return nil
 }
 
 // OperateSyncTCloudCvm ...
@@ -113,6 +217,7 @@ func (svc *syncCvmSvc) OperateSyncTCloudCvm(cts *rest.Contexts) (interface{}, er
 			Limit:  typecore.TCloudQueryLimit,
 		},
 	}
+	cloudCvmTotalMap := make(map[string]struct{}, 0)
 	for {
 		cvms, err := cli.ListCvm(cts.Kit, listOpt)
 		if err != nil {
@@ -126,6 +231,7 @@ func (svc *syncCvmSvc) OperateSyncTCloudCvm(cts *rest.Contexts) (interface{}, er
 
 		cloudIDs := make([]string, 0, len(cvms))
 		for _, one := range cvms {
+			cloudCvmTotalMap[*one.InstanceId] = struct{}{}
 			cloudIDs = append(cloudIDs, *one.InstanceId)
 		}
 
@@ -146,6 +252,11 @@ func (svc *syncCvmSvc) OperateSyncTCloudCvm(cts *rest.Contexts) (interface{}, er
 		}
 
 		listOpt.Page.Offset += typecore.TCloudQueryLimit
+	}
+
+	if err = svc.removeDBNotExistCvm(cts.Kit, cli, req, cloudCvmTotalMap); err != nil {
+		logs.Errorf("remove db not exist cvm failed, err: %v, rid: %s", err, cts.Kit.Rid)
+		return nil, err
 	}
 
 	return nil, nil
