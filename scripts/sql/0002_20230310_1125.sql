421e3add01cb25d10c3b900df56d68e71e4910f0
jamesge <jamesge@tencent.com>
2023-03-17 11:38:06 +0800

    chore: add disk_cvm_rel & eip_cvm_rel Foreign Key (merge request !347)
    Squash merge branch 'add_forkey' into 'v1.0'
    chore: add disk_cvm_rel & eip_cvm_rel Foreign Key
    
    N

--- scripts/sql/0002_20230310_1125.sql
+++ scripts/sql/0002_20230310_1125.sql
@@ -1,3 +1,9 @@
 alter table huawei_region add service varchar(20) not null;
 
 alter table account drop column department_ids;
+
+alter table disk_cvm_rel add constraint DISK_ID foreign key(disk_id) REFERENCES disk(id) ON DELETE CASCADE;
+alter table disk_cvm_rel add constraint CVM_ID foreign key(cvm_id) REFERENCES cvm(id) ON DELETE CASCADE;
+
+alter table eip_cvm_rel add constraint EIP_ID foreign key(eip_id) REFERENCES eip(id) ON DELETE CASCADE;
+alter table eip_cvm_rel add constraint CVM_ID foreign key(cvm_id) REFERENCES cvm(id) ON DELETE CASCADE;
