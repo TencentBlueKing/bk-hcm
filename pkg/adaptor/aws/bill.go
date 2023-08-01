636f2b0a37066e93f06aa911c216e16187747aa5
ironguo <ironguo@tencent.com>
2023-08-01 21:28:30 +0800

    feature: modify aws bill query fields. (merge request !806)
    Squash merge branch 'feature-bill-aws-0801' into 'v1.0'
    feature: modify aws bill query fields.

--- pkg/adaptor/aws/bill.go
+++ pkg/adaptor/aws/bill.go
@@ -42,41 +42,6 @@ import (
 )
 
 const (
-	// QueryBillFields 需要查询的AWS云账单字段
-	QueryBillFields = "identity_line_item_id,identity_time_interval,bill_invoice_id,bill_invoicing_entity," +
-		"bill_billing_entity,bill_bill_type,bill_payer_account_id,bill_billing_period_start_date," +
-		"bill_billing_period_end_date,line_item_usage_account_id,line_item_line_item_type,line_item_usage_start_date," +
-		"line_item_usage_end_date,line_item_product_code,line_item_usage_type,line_item_operation," +
-		"line_item_availability_zone,line_item_resource_id,line_item_usage_amount,line_item_normalization_factor," +
-		"line_item_normalized_usage_amount,line_item_currency_code,line_item_unblended_rate,line_item_unblended_cost," +
-		"line_item_blended_rate,line_item_blended_cost,line_item_line_item_description,line_item_tax_type," +
-		"line_item_net_unblended_rate,line_item_net_unblended_cost,line_item_legal_entity,product_product_name," +
-		"product_purchase_option,product_from_location,product_from_location_type,product_from_region_code," +
-		"product_instance_type,product_instance_type_family,product_location,product_location_type," +
-		"product_marketoption,product_normalization_size_factor,product_operation,product_product_family," +
-		"product_purchaseterm,product_region,product_region_code,product_to_location,product_to_location_type," +
-		"product_to_region_code,product_transfer_type,pricing_lease_contract_length,pricing_offering_class," +
-		"pricing_purchase_option,pricing_currency,pricing_public_on_demand_cost,pricing_public_on_demand_rate," +
-		"pricing_term,pricing_unit,discount_edp_discount,discount_total_discount,product_tenancy," +
-		"product_database_engine,reservation_amortized_upfront_cost_for_usage," +
-		"reservation_amortized_upfront_fee_for_billing_period,reservation_effective_cost,reservation_end_time," +
-		"reservation_modification_status,reservation_net_amortized_upfront_cost_for_usage," +
-		"reservation_net_amortized_upfront_fee_for_billing_period,reservation_net_effective_cost," +
-		"reservation_net_recurring_fee_for_usage,reservation_net_unused_amortized_upfront_fee_for_billing_period," +
-		"reservation_net_unused_recurring_fee,reservation_net_upfront_value," +
-		"reservation_normalized_units_per_reservation,reservation_number_of_reservations," +
-		"reservation_recurring_fee_for_usage,reservation_reservation_a_r_n,reservation_start_time," +
-		"reservation_subscription_id,reservation_total_reserved_normalized_units,reservation_total_reserved_units," +
-		"reservation_units_per_reservation,reservation_unused_amortized_upfront_fee_for_billing_period," +
-		"reservation_unused_normalized_unit_quantity,reservation_unused_quantity,reservation_unused_recurring_fee," +
-		"reservation_upfront_value,savings_plan_total_commitment_to_date,savings_plan_savings_plan_a_r_n," +
-		"savings_plan_savings_plan_rate,savings_plan_savings_plan_effective_cost,savings_plan_used_commitment," +
-		"savings_plan_amortized_upfront_commitment_for_billing_period," +
-		"savings_plan_recurring_commitment_for_billing_period,savings_plan_start_time,savings_plan_end_time," +
-		"savings_plan_offering_type,savings_plan_payment_option,savings_plan_purchase_term,savings_plan_region," +
-		"savings_plan_net_savings_plan_effective_cost," +
-		"savings_plan_net_amortized_upfront_commitment_for_billing_period," +
-		"savings_plan_net_recurring_commitment_for_billing_period,product_servicecode,product_servicename"
 	// QueryBillSQL 查询云账单的SQL
 	QueryBillSQL = "SELECT %s FROM %s.%s %s OFFSET %d LIMIT %d"
 	// QueryBillTotalSQL 查询云账单总数量的SQL
@@ -124,7 +89,7 @@ func (a *Aws) GetBillList(kt *kit.Kit, opt *typesBill.AwsBillListOption,
 		return 0, nil, err
 	}
 
-	sql := fmt.Sprintf(QueryBillSQL, QueryBillFields, billInfo.CloudDatabaseName, billInfo.CloudTableName, where,
+	sql := fmt.Sprintf(QueryBillSQL, "*", billInfo.CloudDatabaseName, billInfo.CloudTableName, where,
 		opt.Page.Offset, opt.Page.Limit)
 	list, err := a.GetAwsAthenaQuery(kt, sql, billInfo)
 	if err != nil {
