0549eca4693816cb1f44f6966b0c8868d6f0adfe
royalpioneer <royalpioneer@foxmail.com>
2023-08-10 14:24:50 +0800

    feature: 添加itsm审批单据搜索

--- front/src/views/service/my-approval/page.tsx
+++ front/src/views/service/my-approval/page.tsx
@@ -1,5 +1,6 @@
 import { useAccountStore } from '@/store';
 import { defineComponent, ref } from 'vue';
+import './index.scss';
 
 export default defineComponent({
   setup() {
@@ -15,7 +16,7 @@ export default defineComponent({
       {
         field: 'current_steps',
         label: '当前步骤',
-        render: ({ cell }: { cell: any}) => cell.map(({ name }: { name: string }) => name).join(','),
+        render: ({ cell }: { cell: any }) => cell.map(({ name }: { name: string }) => name).join(','),
       },
       {
         field: 'current_processors',
@@ -36,17 +37,32 @@ export default defineComponent({
       },
       {
         label: '操作',
-        render: ({ data }) => (
+        render: ({ data }: { data: any }) => (
           <>
-            <bk-button text theme='primary' class='mr5' onClick={() => handleApprove(data)}>通过</bk-button>
-            <bk-button text theme='primary' onClick={() => handleRefuse(data)}>拒绝</bk-button>
+            <bk-button
+              text
+              theme='primary'
+              class='mr5'
+              onClick={() => handleApprove(data)}>
+              通过
+            </bk-button>
+            <bk-button text theme='primary' onClick={() => handleRefuse(data)}>
+              拒绝
+            </bk-button>
           </>
         ),
       },
     ];
 
     const datas = ref([]);
+    const filter = ref({ op: 'and', rules: [] });
     const accountStore = useAccountStore();
+    const isLoading = ref(false);
+    const pagination = ref({
+      count: false,
+      limit: 20,
+      start: 0,
+    });
 
     const handleApprove = async (data: any) => {
       await accountStore.approveTickets({
@@ -69,25 +85,33 @@ export default defineComponent({
     };
 
     const getList = async () => {
+      isLoading.value = true;
       const { data } = await accountStore.getApprovalList({
-        filter: { op: 'and', rules: [] },
-        page: {
-          count: false,
-          limit: 20,
-          start: 0,
-        },
+        filter: filter.value,
+        page: pagination.value,
       });
       datas.value = data;
+      isLoading.value = false;
     };
 
     getList();
 
     return () => (
       <div>
-        <bk-table
-          columns={tableColumns}
-          data={datas.value}
-        ></bk-table>
+        <div class={'my-approval-selector-container'}>
+          {/* <bk-search-select
+            class="bg-white w280 my-approval-selector"
+            placeholder={'通过单号、标题搜索'}
+            data={searchData}
+            v-model={searchValue.value}
+          /> */}
+        </div>
+        <bk-loading loading={isLoading.value}>
+          <bk-table
+            columns={tableColumns}
+            data={datas.value}
+          />
+        </bk-loading>
       </div>
     );
   },
