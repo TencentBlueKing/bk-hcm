348894c275979ee5580d66c6014d9cae237c88b2
tiger-guo <1774687347@qq.com>
2023-08-10 18:05:20 +0800

    fix：合并0010sql文件

--- scripts/sql/9999.sql
+++ scripts/sql/9999.sql
@@ -1,9 +1,12 @@
 /*
-   SQLVER=0010,HCMVER=v1.1.23
+    SQLVER=9999,HCMVER=v9.9.9
 
-   Notes:
-       1. 添加子账号表。
+    Notes:
+        1. 添加子账号表。
+        2. 修复aws_security_group_rule表memo字段错误的非空约束
+        3. aws_region表增加account_id字段，独立存储aws每个账号的地域信息
 */
+start transaction;
 
 create table if not exists `sub_account`
 (
@@ -29,3 +32,22 @@ create table if not exists `sub_account`
 
 insert into id_generator(`resource`, `max_id`)
 values ('sub_account', '0');
+
+alter table aws_security_group_rule
+    modify column memo varchar(255) default '';
+
+delete
+from aws_region;
+alter table aws_region
+    add column account_id varchar(64) not null;
+
+alter table aws_region
+    drop index `idx_uk_region_id_status`;
+
+alter table aws_region
+    add unique key `idx_uk_account_id_region_id` (`account_id`, `region_id`);
+
+CREATE OR REPLACE VIEW `hcm_version`(`hcm_ver`, `sql_ver`) AS
+SELECT 'v9.9.9' as `hcm_ver`, '9999' as `sql_ver`;
+
+commit;
