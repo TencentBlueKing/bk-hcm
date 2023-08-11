f2c4eb8a635c807f4bb4063585e46b2f77521df5
brookszhang <857324109@qq.com>
2023-08-11 11:05:40 +0800

    feature: iass层资源同步状态等信息文档

--- scripts/sql/9999.sql
+++ scripts/sql/9999.sql
@@ -8,6 +8,9 @@
 */
 start transaction;
 
+CREATE OR REPLACE VIEW `hcm_version`(`hcm_ver`, `sql_ver`) AS
+SELECT 'v9.9.9' as `hcm_ver`, '9999' as `sql_ver`;
+
 create table if not exists `sub_account`
 (
     `id`         varchar(64)  not null,
@@ -47,7 +50,23 @@ alter table aws_region
 alter table aws_region
     add unique key `idx_uk_account_id_region_id` (`account_id`, `region_id`);
 
-CREATE OR REPLACE VIEW `hcm_version`(`hcm_ver`, `sql_ver`) AS
-SELECT 'v9.9.9' as `hcm_ver`, '9999' as `sql_ver`;
+create table if not exists `sync`
+(
+    `id`         varchar(64)  not null,
+    `vendor`     varchar(16)  not null,
+    `account_id` varchar(64)  not null,
+    `res_name`   varchar(64)  not null,
+    `res_status` varchar(64)  not null,
+    `res_end_time` timestamp  default '',
+    `res_failed_reason` varchar(255)  not null,
+    `creator`    varchar(64)  not null,
+    `reviser`    varchar(64)  not null,
+    `created_at` timestamp    not null default current_timestamp,
+    `updated_at` timestamp    not null default current_timestamp on update current_timestamp,
+    primary key (`id`),
+    unique key `idx_uk_vendor_account_id` (`vendor`, `account_id`)
+) engine = innodb
+  default charset = utf8mb4
+  COLLATE utf8mb4_bin;
 
 commit;
