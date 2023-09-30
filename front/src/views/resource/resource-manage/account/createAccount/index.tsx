4c8b174a47c2580ceb39ac661dd9186759d3b157
royalpioneer <royalpioneer@foxmail.com>
2023-09-30 15:07:02 +0800

    fix: 调整购买主机布局错位的问题

--- front/src/views/resource/resource-manage/account/createAccount/index.tsx
+++ front/src/views/resource/resource-manage/account/createAccount/index.tsx
@@ -56,6 +56,7 @@ export default defineComponent({
       <Dialog
         fullscreen
         isShow={props.isShow}
+        onClosed={props.onCancel}
         class={'create-account-dialog-container'}>
         {{
           tools: () => (
