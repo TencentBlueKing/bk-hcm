7e09735d1bd0323c4c14d4b66314bc486df73149
royalpioneer <royalpioneer@foxmail.com>
2023-10-08 16:56:36 +0800

    feature: 批量开机/关机/重启/回收

--- front/src/views/resource/resource-manage/common/table/HostOperations.tsx
+++ front/src/views/resource/resource-manage/common/table/HostOperations.tsx
@@ -1,11 +1,12 @@
-import { Button, Dialog, Dropdown, Message } from 'bkui-vue';
-import { PropType, computed, defineComponent, ref, watch } from 'vue';
+import { Button, Checkbox, Dialog, Dropdown, Message } from 'bkui-vue';
+import { PropType, computed, defineComponent, reactive, ref, watch } from 'vue';
 import './index.scss';
 import { usePreviousState } from '@/hooks/usePreviousState';
 import { useResourceStore } from '@/store';
 import { AngleDown } from 'bkui-vue/lib/icon';
 import { BkDropdownItem, BkDropdownMenu } from 'bkui-vue/lib/dropdown';
-import { useLocalTable } from '@/hooks/useLocalTable';
+import CommonLocalTable from '../commonLocalTable';
+import { BkButtonGroup } from 'bkui-vue/lib/button';
 
 export enum Operations {
   None = 'none',
@@ -55,7 +56,12 @@ export default defineComponent({
     const dialogRef = ref(null);
     const isConfirmDisabled = ref(true);
     const targetHost = ref([]);
+    const unTargetHost = ref([]);
     const isLoading = ref(false);
+    const tableData = ref([]);
+    const selected = ref('target');
+    const withDiskSet = ref(new Set());
+    const withEipSet = ref(new Set());
 
     const previousOperationType = usePreviousState(operationType);
     const resourceStore = useResourceStore();
@@ -68,6 +74,88 @@ export default defineComponent({
       if (operationType.value === Operations.None) return `批量${OperationsMap[previousOperationType.value]}`;
       return `批量${OperationsMap[operationType.value]}`;
     });
+
+    const searchData = reactive([
+      {
+        name: '地域',
+        id: 'region',
+      },
+      {
+        name: '可用区',
+        id: 'zone',
+      },
+      {
+        name: '状态',
+        id: 'status',
+      },
+    ]);
+
+    const computedColumns = computed(() => [
+      {
+        field: '_private_ip',
+        label: '内网IP',
+        render: ({ data }: any) => ([...data.private_ipv4_addresses, ...data.private_ipv6_addresses].join(',') || '--'),
+      },
+      {
+        field: '_public_ip',
+        label: '外网IP',
+        render: ({ data }: any) => ([...data.public_ipv4_addresses, ...data.public_ipv6_addresses].join(',') || '--'),
+      },
+      {
+        field: 'name',
+        label: '主机名称',
+      },
+      {
+        field: 'region',
+        label: '地域',
+      },
+      {
+        field: 'zone',
+        label: '可用区',
+      },
+      {
+        field: 'status',
+        label: '状态',
+      },
+      {
+        field: 'machine_type',
+        label: '机型',
+      },
+      {
+        field: '_with_disk',
+        label: '云硬盘随主机回收',
+        render: ({ data }: any) => (
+          <div>
+            <Checkbox
+              checked={withDiskSet.value.has(data.id)}
+              onChange={(isChecked) => {
+                if (isChecked) withDiskSet.value.add(data.id);
+                else withDiskSet.value.delete(data.id);
+              }}
+            >
+              1 个数据盘
+            </Checkbox>
+          </div>
+        ),
+      },
+      {
+        field: '_with_eip',
+        label: '弹性 IP 随主机回收',
+        render: ({ data }: any) => (
+          <Checkbox
+            checked={withEipSet.value.has(data.id)}
+            onChange={(isChecked) => {
+              if (isChecked) withEipSet.value.add(data.id);
+              else withEipSet.value.delete(data.id);
+            }}
+          >
+            {
+              [...data.public_ipv4_addresses, ...data.public_ipv6_addresses].join(',') || '--'
+            }
+          </Checkbox>
+        ),
+      },
+    ].filter(({ field }) => !['_with_disk', '_with_eip'].includes(field) || operationType.value === Operations.Recycle));
 
     // const computedPreviousOperationType = computed(() => {
     //   switch (operationType.value) {
@@ -91,101 +179,118 @@ export default defineComponent({
         if (operationType.value === Operations.None) return;
 
         const runningHosts = [];
+        const unRunningHosts = [];
+
         const shutdownHosts = [];
+        const unShutdownHosts = [];
 
         for (const host of props.selections) {
           const { status } = host;
           if (HOST_RUNNING_STATUS.includes(status)) runningHosts.push(host);
+          else unRunningHosts.push(host);
+
           if (HOST_SHUTDOWN_STATUS.includes(status)) shutdownHosts.push(host);
+          else unShutdownHosts.push(host);
         }
 
         switch (operationType.value) {
           case Operations.Open: {
             targetHost.value = shutdownHosts;
+            unTargetHost.value = unShutdownHosts;
             break;
           }
           case Operations.Close:
           case Operations.Reboot: {
             targetHost.value = runningHosts;
+            unTargetHost.value = unRunningHosts;
             break;
           }
           case Operations.Recycle: {
             targetHost.value = [...runningHosts, ...shutdownHosts];
+            unTargetHost.value = [...unRunningHosts, ...unShutdownHosts];
           }
         }
         isConfirmDisabled.value = targetHost.value.length === 0;
       },
     );
 
-    // const computedContent = computed(() => {
-    //   const allHostsNum = props.selections.length;
-    //   const targetHostsNum = targetHost.value.length;
-    //   const targetOperationName = OperationsMap[operationType.value];
-    //   let oppositeOperationName = '';
-    //   switch (operationType.value) {
-    //     case Operations.Open: {
-    //       oppositeOperationName = OperationsMap[Operations.Close];
-    //       break;
-    //     }
-    //     case Operations.Close:
-    //     case Operations.Reboot: {
-    //       oppositeOperationName = OperationsMap[Operations.Open];
-    //       break;
-    //     }
-    //     case Operations.Recycle: {
-    //       oppositeOperationName = `${OperationsMap[Operations.Open]}、${OperationsMap[Operations.Close]}`;
-    //       break;
-    //     }
-    //   }
-    //   if (targetHostsNum === 0) {
-    //     return (
-    //       <p>
-    //         您已选择了 {allHostsNum} 台主机进行
-    //         {targetOperationName}操作, 其中
-    //         <span class={'host_operations_blue_txt'}> {allHostsNum} </span>
-    //         台是已{computedPreviousOperationType.value}的，不支持对其操作。
-    //         <br />
-    //         <span class={'host_operations_red_txt'}>
-    //           由于所选主机均处于{targetOperationName}
-    //           状态,无法进行操作。
-    //         </span>
-    //       </p>
-    //     );
-    //   }
-    //   if (targetHostsNum === allHostsNum) {
-    //     return (
-    //       <p>
-    //         您已选择了 {allHostsNum} 台主机进行
-    //         {targetOperationName}操作,本次操作将对
-    //         <span class={'host_operations_blue_txt'}> {allHostsNum} </span>
-    //         台处于{oppositeOperationName}
-    //         状态的主机进行{targetOperationName}操作。
-    //         <br />
-    //         <span class={'host_operations_red_txt'}>
-    //           请确认您所选择的目标是正确的，该操作将对主机进行
-    //           {targetOperationName}操作。
-    //         </span>
-    //       </p>
-    //     );
-    //   }
-    //   if (allHostsNum > targetHostsNum) {
-    //     return (
-    //       <p>
-    //         您已选择了 {allHostsNum} 台主机进行
-    //         {targetOperationName}。本次操作将对
-    //         <span class={'host_operations_blue_txt'}> {targetHostsNum} </span>
-    //         台处于{oppositeOperationName}状态的主机进行
-    //         {targetOperationName}，其余主机的状态不支持{targetOperationName}。
-    //         <br />
-    //         <span class={'host_operations_red_txt'}>
-    //           请确认您所选择的目标是正确的,该操作将对主机进行
-    //           {targetOperationName}操作
-    //         </span>
-    //       </p>
-    //     );
-    //   }
-    //   return '';
-    // });
+    const computedContent = computed(() => {
+      const targetOperationName = OperationsMap[operationType.value];
+      // let oppositeOperationName = '';
+      // switch (operationType.value) {
+      //   case Operations.Open: {
+      //     oppositeOperationName = OperationsMap[Operations.Close];
+      //     break;
+      //   }
+      //   case Operations.Close:
+      //   case Operations.Reboot: {
+      //     oppositeOperationName = OperationsMap[Operations.Open];
+      //     break;
+      //   }
+      //   case Operations.Recycle: {
+      //     oppositeOperationName = `${OperationsMap[Operations.Open]}、${OperationsMap[Operations.Close]}`;
+      //     break;
+      //   }
+      // }
+      return (
+        <p class={'host-operations-selection-tip'}>
+          已选择 <span class={'host-operations-selection-tip-all'}>{ props.selections.length }</span> 个主机，
+          其中可{ targetOperationName } <span class={'host-operations-selection-tip-target'}>{ targetHost.value.length }</span> 个，
+          不可{ targetOperationName } <span class={'host-operations-selection-tip-untarget'}>{ unTargetHost.value.length }</span> 个。
+          <Button text theme='primary'>
+            查看{targetOperationName}规则
+          </Button>
+        </p>
+      );
+      // if (targetHostsNum === 0) {
+      //   return (
+      //     <p>
+      //       您已选择了 {allHostsNum} 台主机进行
+      //       {targetOperationName}操作, 其中
+      //       <span class={'host_operations_blue_txt'}> {allHostsNum} </span>
+      //       台是已{computedPreviousOperationType.value}的，不支持对其操作。
+      //       <br />
+      //       <span class={'host_operations_red_txt'}>
+      //         由于所选主机均处于{targetOperationName}
+      //         状态,无法进行操作。
+      //       </span>
+      //     </p>
+      //   );
+      // }
+      // if (targetHostsNum === allHostsNum) {
+      //   return (
+      //     <p>
+      //       您已选择了 {allHostsNum} 台主机进行
+      //       {targetOperationName}操作,本次操作将对
+      //       <span class={'host_operations_blue_txt'}> {allHostsNum} </span>
+      //       台处于{oppositeOperationName}
+      //       状态的主机进行{targetOperationName}操作。
+      //       <br />
+      //       <span class={'host_operations_red_txt'}>
+      //         请确认您所选择的目标是正确的，该操作将对主机进行
+      //         {targetOperationName}操作。
+      //       </span>
+      //     </p>
+      //   );
+      // }
+      // if (allHostsNum > targetHostsNum) {
+      //   return (
+      //     <p>
+      //       您已选择了 {allHostsNum} 台主机进行
+      //       {targetOperationName}。本次操作将对
+      //       <span class={'host_operations_blue_txt'}> {targetHostsNum} </span>
+      //       台处于{oppositeOperationName}状态的主机进行
+      //       {targetOperationName}，其余主机的状态不支持{targetOperationName}。
+      //       <br />
+      //       <span class={'host_operations_red_txt'}>
+      //         请确认您所选择的目标是正确的,该操作将对主机进行
+      //         {targetOperationName}操作
+      //       </span>
+      //     </p>
+      //   );
+      // }
+      // return '';
+    });
 
     const handleConfirm = async () => {
       try {
@@ -195,7 +300,11 @@ export default defineComponent({
           theme: 'warning',
         });
         if (operationType.value === Operations.Recycle) {
-          const hostIds = targetHost.value.map(v => ({ id: v.id })) as Array<Record<string, string>>;
+          const hostIds = targetHost.value.map(v => ({
+            id: v.id,
+            with_disk: withDiskSet.value.has(v.id),
+            with_eip: withEipSet.value.has(v.id),
+          }));
           await resourceStore.recycledCvmsData({ infos: hostIds });
         } else {
           const hostIds = targetHost.value.map(v => v.id);
@@ -219,63 +328,25 @@ export default defineComponent({
 
     const operationsDisabled = computed(() => !props.selections.length);
 
-    const { CommonLocalTable } = useLocalTable({
-      data: props.selections,
-      columns: [
-        {
-          field: '_private_ip',
-          label: '内网IP',
-          render: ({ data }) => ([...data.private_ipv4_addresses, ...data.private_ipv6_addresses].join(',') || '--'),
-        },
-        {
-          field: '_public_ip',
-          label: '外网IP',
-          render: ({ data }) => ([...data.public_ipv4_addresses, ...data.public_ipv6_addresses].join(',') || '--'),
-        },
-        {
-          field: 'name',
-          label: '主机名称',
-        },
-        {
-          field: 'region',
-          label: '地域',
-        },
-        {
-          field: 'zone',
-          label: '可用区',
-        },
-        {
-          field: 'status',
-          label: '状态',
-        },
-        {
-          field: 'machine_type',
-          label: '机型',
-        },
-        {
-          field: '_with_disk',
-          label: '云硬盘随主机回收',
-        },
-        {
-          field: '_with_eip',
-          label: '弹性 IP 随主机回收',
-        },
-      ],
-      searchData: [
-        {
-          name: '地域',
-          id: 'region',
-        },
-        {
-          name: '可用区',
-          id: 'zone',
-        },
-        {
-          name: '状态',
-          id: 'status',
-        },
-      ],
-    });
+    watch(
+      () => props.selections,
+      () => {
+        handleSwitch(true);
+      },
+      {
+        deep: true,
+      },
+    );
+
+    const handleSwitch = (isTarget = true) => {
+      if (isTarget) {
+        tableData.value = targetHost.value;
+        selected.value = 'target';
+      } else {
+        tableData.value = unTargetHost.value;
+        selected.value = 'untarget';
+      }
+    };
 
     return () => (
       <>
@@ -306,7 +377,8 @@ export default defineComponent({
 
         <Dialog
           isShow={isDialogShow.value}
-          quick-close={!isLoading.value}
+          // quick-close={!isLoading.value}
+          quickClose={false}
           onClosed={() => (operationType.value = Operations.None)}
           onConfirm={handleConfirm}
           title={computedTitle.value}
@@ -315,7 +387,31 @@ export default defineComponent({
           closeIcon={!isLoading.value}>
           {{
             default: () => (
-             <CommonLocalTable/>
+             <div>
+                <p>
+                  { computedContent.value }
+                </p>
+                <CommonLocalTable
+                  data={tableData.value}
+                  columns={computedColumns.value}
+                  searchData={searchData}
+                >
+                <BkButtonGroup>
+                  <Button
+                    onClick={() => handleSwitch(true)}
+                    selected={selected.value === 'target'}
+                  >
+                    可{OperationsMap[operationType.value]}
+                  </Button>
+                  <Button
+                    onClick={() => handleSwitch(false)}
+                    selected={selected.value === 'untarget'}
+                  >
+                    不可{OperationsMap[operationType.value]}
+                  </Button>
+                </BkButtonGroup>
+              </CommonLocalTable>
+             </div>
             ),
             footer: (
               <>
