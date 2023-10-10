d28f982ff18283d1847d5c39f44f99e2bdface97
tiger-guo <1774687347@qq.com>
2023-10-10 18:05:11 +0800

    feature: 添加资源下查询配额接口

--- pkg/tools/hooks/handler/biz.go
+++ pkg/tools/hooks/handler/biz.go
@@ -52,7 +52,7 @@ func BizValidWithAuth(cts *rest.Contexts, opt *ValidWithAuthOption) error {
 	authRes := make([]meta.ResourceAttribute, 0, total)
 	notMatchedIDs, recycledIDs, notRecycledIDS := make([]string, 0), make([]string, 0, total), make([]string, 0, total)
 	for id, info := range opt.BasicInfos {
-		if info.BkBizID != bizID {
+		if info.BkBizID != 0 && bizID != 0 && info.BkBizID != bizID {
 			notMatchedIDs = append(notMatchedIDs, id)
 		}
 		if info.RecycleStatus == enumor.RecycleStatus {
