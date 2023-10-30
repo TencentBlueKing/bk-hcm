fa331ee87214d1dc7cbd374dcce6689141270e7a
coder-cwc <17502088283@163.com>
2023-10-30 11:29:46 +0800

    fix: 购买弹性ip，数量可以是负数

--- front/src/views/business/forms/eip/tcloud.vue
+++ front/src/views/business/forms/eip/tcloud.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
-import {
-  ref,
-  watch,
-  defineEmits,
-  defineExpose,
-} from 'vue';
+import { ref, watch, defineEmits, defineExpose } from 'vue';
 
 const emit = defineEmits(['change']);
 
@@ -12,14 +7,14 @@ const formData = ref({
   eip_name: '',
   eip_count: 1,
   service_provider: 'BGP',
-  address_type: 'EIP'
-})
+  address_type: 'EIP',
+});
 const formRef = ref(null);
 const rules = {
   eip_count: [
     {
       validator: (value: number) => value > 0,
-      message: '数量必填',
+      message: '请输入大于0的整数',
       trigger: 'blur',
     },
   ],
@@ -29,58 +24,35 @@ const rules = {
       message: '名称必填',
       trigger: 'blur',
     },
-  ]
+  ],
 };
 
-
 const handleChange = () => {
-  emit('change', formData.value)
-}
+  emit('change', formData.value);
+};
 
 const validate = () => {
-  return formRef.value.validate()
-}
+  return formRef.value.validate();
+};
 
-watch(
-  () => formData,
-  handleChange,
-  {
-    immediate: true,
-    deep: true
-  }
-)
+watch(() => formData, handleChange, {
+  immediate: true,
+  deep: true,
+});
 
 defineExpose([validate]);
 </script>
 
 <template>
-  <bk-form
-    ref="formRef"
-    :model="formData"
-    :rules="rules"
-  >
-    <bk-form-item
-      label="名称"
-      property="eip_name"
-      required
-    >
+  <bk-form ref="formRef" :model="formData" :rules="rules">
+    <bk-form-item label="名称" property="eip_name" required>
       <bk-input v-model="formData.eip_name" placeholder="请输入名称" />
     </bk-form-item>
-    <bk-form-item
-      label="IP地址类型"
-      required
-    >
-      <bk-radio
-        model-value="常规BGP IP"
-        label="常规BGP IP"
-      />
+    <bk-form-item label="IP地址类型" required>
+      <bk-radio model-value="常规BGP IP" label="常规BGP IP" />
     </bk-form-item>
-    <bk-form-item
-      label="数量"
-      property="eip_count"
-      required
-    >
-      <bk-input v-model="formData.eip_count" type="number" placeholder="请输入数量" />
+    <bk-form-item label="数量" property="eip_count" required>
+      <bk-input v-model="formData.eip_count" placeholder="请输入数量" />
     </bk-form-item>
   </bk-form>
 </template>
