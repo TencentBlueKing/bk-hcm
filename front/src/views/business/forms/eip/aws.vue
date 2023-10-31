216e4afa41794bb0f87d10712e6101b4e3ec5d74
coder-cwc <17502088283@163.com>
2023-10-31 17:10:43 +0800

    fix: 弹性IP的带宽大小没有做限制

--- front/src/views/business/forms/eip/aws.vue
+++ front/src/views/business/forms/eip/aws.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
-import {
-  ref,
-  watch,
-  defineEmits,
-  defineExpose,
-} from 'vue';
+import { ref, watch, defineEmits, defineExpose } from 'vue';
 
 const props = defineProps({
   region: {
-    type: String
-  }
+    type: String,
+  },
 });
 
 const emit = defineEmits(['change']);
@@ -18,54 +13,34 @@ const formData = ref({
   public_ipv4_pool: '',
   network_border_group: props.region,
   eip_count: 1,
-})
+});
 const formRef = ref(null);
 
-
 const handleChange = () => {
-  formData.value.network_border_group = props.region
-  emit('change', formData.value)
-}
+  formData.value.network_border_group = props.region;
+  emit('change', formData.value);
+};
 
 const validate = () => {
-  return formRef.value.validate()
-}
+  return formRef.value.validate();
+};
 
-watch(
-  () => formData,
-  handleChange,
-  {
-    immediate: true,
-    deep: true
-  }
-)
+watch(() => formData, handleChange, {
+  immediate: true,
+  deep: true,
+});
 
 defineExpose([validate]);
 </script>
 
 <template>
-  <bk-form
-    ref="formRef"
-    :model="formData"
-  >
-    <bk-form-item
-      label="网络边界组"
-    >
+  <bk-form ref="formRef" :model="formData">
+    <bk-form-item label="网络边界组" required>
       {{ region }}
     </bk-form-item>
-    <bk-form-item
-      label="公有IPv4地址池"
-    >
-      <bk-radio
-        model-value="Amazon的IPv4地址池"
-        label="Amazon的IPv4地址池"
-      />
+    <bk-form-item label="公有IPv4地址池">
+      <bk-radio model-value="Amazon的IPv4地址池" label="Amazon的IPv4地址池" />
     </bk-form-item>
-    <bk-form-item
-      label="数量"
-      required
-    >
-      1
-    </bk-form-item>
+    <bk-form-item label="数量" required>1</bk-form-item>
   </bk-form>
 </template>
