b288bbbbd1c1b4b856e9cfca08eb8a6882209fc8
jojo <82189108+royalpioneer@users.noreply.github.com>
2023-11-22 16:01:53 +0800

    fix: 测试问题修复 (#410)
    * fix: 账号接入新增下一步&完成后重新打开返回第一页
    
    * fix: 购买华为云主机时实例名称填写-，页面老是弹框提示报错
    
    * fix: 子网管理页不展示操作列

--- front/src/views/resource/resource-manage/children/manage/subnet-manage.vue
+++ front/src/views/resource/resource-manage/children/manage/subnet-manage.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import type { FilterType } from '@/typings/resource';
-import { PropType, defineExpose, h, computed, withDirectives } from 'vue';
-import { Button, Message, InfoBox, bkTooltips } from 'bkui-vue';
-import { useResourceStore } from '@/store/resource';
+import { PropType, defineExpose, computed } from 'vue';
+// import { Message, InfoBox } from 'bkui-vue';
+// import { useResourceStore } from '@/store/resource';
 
 import useColumns from '../../hooks/use-columns';
 import useQueryList from '../../hooks/use-query-list';
@@ -12,7 +12,6 @@ import {
   BatchDistribution,
   DResourceType,
 } from '@/views/resource/resource-manage/children/dialog/batch-distribution';
-import { Senarios, useWhereAmI } from '@/hooks/useWhereAmI';
 
 const props = defineProps({
   filter: {
@@ -31,9 +30,8 @@ const props = defineProps({
 
 const { selections, handleSelectionChange, resetSelections } = useSelection();
 
-const resourceStore = useResourceStore();
+// const resourceStore = useResourceStore();
 const { columns, settings } = useColumns('subnet');
-const { whereAmI } = useWhereAmI();
 
 const { searchData, searchValue, filter } = useFilter(props);
 
@@ -78,157 +76,157 @@ const hostSearchData = computed(() => {
   ];
 });
 
-const emit = defineEmits(['auth']);
+// const emit = defineEmits(['auth']);
 
 // 抛出请求数据的方法，新增成功使用
 const fetchComponentsData = () => {
   handlePageChange(1);
 };
 
-const handleDeleteSubnet = (data: any) => {
-  if (data.vendor === 'gcp') {
-    const subnetIds = [data.id];
-    const getRelateNum = (type: string, field = 'subnet_id', op = 'in') => {
-      return resourceStore.list(
-        {
-          page: {
-            count: true,
-          },
-          filter: {
-            op: 'and',
-            rules: [
-              {
-                field,
-                op,
-                value: subnetIds,
-              },
-            ],
-          },
-        },
-        type,
-      );
-    };
-    Promise.all([
-      getRelateNum('cvms', 'subnet_ids', 'json_overlaps'),
-      getRelateNum('network_interfaces'),
-    ]).then(([cvmsResult, networkResult]: any) => {
-      if (cvmsResult?.data?.count || networkResult?.data?.count) {
-        const getMessage = (result: any, name: string) => {
-          if (result?.data?.count) {
-            return `${result?.data?.count}个${name}，`;
-          }
-          return '';
-        };
-        Message({
-          theme: 'error',
-          message: `该子网（name：${data.name}，id：${
-            data.id
-          }）关联${getMessage(cvmsResult, 'CVM')}${getMessage(
-            networkResult,
-            '网络接口',
-          )}不能删除`,
-        });
-      } else {
-        handledelete(data);
-      }
-    });
-  } else {
-    resourceStore.countSubnetIps(data.id as string).then((res: any) => {
-      if (res?.data?.used_ip_count) {
-        Message({
-          theme: 'error',
-          message: `该子网（name：${data.name}，id：${data.id} IPv4已经被使用${res?.data?.used_ip_count}不能删除`,
-        });
-      } else {
-        handledelete(data);
-      }
-    });
-  }
-};
+// const handleDeleteSubnet = (data: any) => {
+//   if (data.vendor === 'gcp') {
+//     const subnetIds = [data.id];
+//     const getRelateNum = (type: string, field = 'subnet_id', op = 'in') => {
+//       return resourceStore.list(
+//         {
+//           page: {
+//             count: true,
+//           },
+//           filter: {
+//             op: 'and',
+//             rules: [
+//               {
+//                 field,
+//                 op,
+//                 value: subnetIds,
+//               },
+//             ],
+//           },
+//         },
+//         type,
+//       );
+//     };
+//     Promise.all([
+//       getRelateNum('cvms', 'subnet_ids', 'json_overlaps'),
+//       getRelateNum('network_interfaces'),
+//     ]).then(([cvmsResult, networkResult]: any) => {
+//       if (cvmsResult?.data?.count || networkResult?.data?.count) {
+//         const getMessage = (result: any, name: string) => {
+//           if (result?.data?.count) {
+//             return `${result?.data?.count}个${name}，`;
+//           }
+//           return '';
+//         };
+//         Message({
+//           theme: 'error',
+//           message: `该子网（name：${data.name}，id：${
+//             data.id
+//           }）关联${getMessage(cvmsResult, 'CVM')}${getMessage(
+//             networkResult,
+//             '网络接口',
+//           )}不能删除`,
+//         });
+//       } else {
+//         handledelete(data);
+//       }
+//     });
+//   } else {
+//     resourceStore.countSubnetIps(data.id as string).then((res: any) => {
+//       if (res?.data?.used_ip_count) {
+//         Message({
+//           theme: 'error',
+//           message: `该子网（name：${data.name}，id：${data.id} IPv4已经被使用${res?.data?.used_ip_count}不能删除`,
+//         });
+//       } else {
+//         handledelete(data);
+//       }
+//     });
+//   }
+// };
 
-const handledelete = (data: any) => {
-  InfoBox({
-    title: '请确认是否删除',
-    subTitle: `将删除【${data.cloud_id}${data.name ? ` - ${data.name}` : ''}】`,
-    theme: 'danger',
-    headerAlign: 'center',
-    footerAlign: 'center',
-    contentAlign: 'center',
-    extCls: 'delete-resource-infobox',
-    onConfirm() {
-      resourceStore
-        .deleteBatch('subnets', {
-          ids: [data.id],
-        })
-        .then(() => {
-          Message({
-            theme: 'success',
-            message: '删除成功',
-          });
-          triggerApi();
-        });
-    },
-  });
-};
+// const handledelete = (data: any) => {
+//   InfoBox({
+//     title: '请确认是否删除',
+//     subTitle: `将删除【${data.cloud_id}${data.name ? ` - ${data.name}` : ''}】`,
+//     theme: 'danger',
+//     headerAlign: 'center',
+//     footerAlign: 'center',
+//     contentAlign: 'center',
+//     extCls: 'delete-resource-infobox',
+//     onConfirm() {
+//       resourceStore
+//         .deleteBatch('subnets', {
+//           ids: [data.id],
+//         })
+//         .then(() => {
+//           Message({
+//             theme: 'success',
+//             message: '删除成功',
+//           });
+//           triggerApi();
+//         });
+//     },
+//   });
+// };
 
-const generateTooltipsOptions = (data: any) => {
-  const action_name = props.isResourcePage ? 'iaas_resource_delete' : 'biz_iaas_resource_delete';
+// const generateTooltipsOptions = (data: any) => {
+//   const action_name = props.isResourcePage ? 'iaas_resource_delete' : 'biz_iaas_resource_delete';
 
-  if (!props.authVerifyData?.permissionAction[action_name]) return {
-    content: '当前用户无权限操作该按钮',
-    disabled: props.authVerifyData?.permissionAction[action_name],
-  };
-  if (props.isResourcePage && data?.bk_biz_id !== -1) return {
-    content: '该子网已分配到业务，仅可在业务下操作',
-    disabled: data.bk_biz_id === -1,
-  };
+//   if (!props.authVerifyData?.permissionAction[action_name]) return {
+//     content: '当前用户无权限操作该按钮',
+//     disabled: props.authVerifyData?.permissionAction[action_name],
+//   };
+//   if (props.isResourcePage && data?.bk_biz_id !== -1) return {
+//     content: '该子网已分配到业务，仅可在业务下操作',
+//     disabled: data.bk_biz_id === -1,
+//   };
 
-  return {
-    disabled: true,
-  };
-};
+//   return {
+//     disabled: true,
+//   };
+// };
 
 const renderColumns = [
   ...columns,
-  {
-    label: '操作',
-    render({ data }: any) {
-      return h(h(
-        'span',
-        {
-          onClick() {
-            emit(
-              'auth',
-              props.isResourcePage
-                ? 'iaas_resource_delete'
-                : 'biz_iaas_resource_delete',
-            );
-          },
-        },
-        [
-          withDirectives(h(
-            Button,
-            {
-              text: true,
-              theme: 'primary',
-              disabled:
-                    !props.authVerifyData?.permissionAction[
-                      props.isResourcePage
-                        ? 'iaas_resource_delete'
-                        : 'biz_iaas_resource_delete'
-                    ] || (whereAmI.value !== Senarios.business && data.bk_biz_id !== -1),
-              onClick() {
-                handleDeleteSubnet(data);
-              },
-            },
-            ['删除'],
-          ), [
-            [bkTooltips, generateTooltipsOptions(data)],
-          ]),
-        ],
-      ));
-    },
-  },
+  // {
+  //   label: '操作',
+  //   render({ data }: any) {
+  //     return h(h(
+  //       'span',
+  //       {
+  //         onClick() {
+  //           emit(
+  //             'auth',
+  //             props.isResourcePage
+  //               ? 'iaas_resource_delete'
+  //               : 'biz_iaas_resource_delete',
+  //           );
+  //         },
+  //       },
+  //       [
+  //         withDirectives(h(
+  //           Button,
+  //           {
+  //             text: true,
+  //             theme: 'primary',
+  //             disabled:
+  //                   !props.authVerifyData?.permissionAction[
+  //                     props.isResourcePage
+  //                       ? 'iaas_resource_delete'
+  //                       : 'biz_iaas_resource_delete'
+  //                   ] || (whereAmI.value !== Senarios.business && data.bk_biz_id !== -1),
+  //             onClick() {
+  //               handleDeleteSubnet(data);
+  //             },
+  //           },
+  //           ['删除'],
+  //         ), [
+  //           [bkTooltips, generateTooltipsOptions(data)],
+  //         ]),
+  //       ],
+  //     ));
+  //   },
+  // },
 ];
 
 defineExpose({ fetchComponentsData });
