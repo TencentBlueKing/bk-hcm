e97fe056084811b808f82515f5a5e52b65393a1f
royalpioneer <royalpioneer@foxmail.com>
2023-11-23 16:23:30 +0800

    feature: 推荐方案预览页整体布局

---
+++ front/src/views/scheme/components/scheme-preview/index.tsx
@@ -0,0 +1,60 @@
+import { Button, Select } from 'bkui-vue';
+import { defineComponent, ref } from 'vue';
+import './index.scss';
+import { Info } from 'bkui-vue/lib/icon';
+
+const { Option } = Select;
+
+export const SchemeSortOptions = [
+  {
+    key: 1,
+    val: '按综合评分排序',
+  },
+  {
+    key: 2,
+    val: '按网络评分排序',
+  },
+  {
+    key: 3,
+    val: '按方案成本排序',
+  },
+];
+
+export default defineComponent({
+  setup() {
+    const sortChoice = ref(SchemeSortOptions[0].key);
+    return () => <div class={'scheme-preview-container'}>
+      <div class={'scheme-preview-header'}>
+        <div class={'scheme-preview-header-title'}>
+          推荐方案
+        </div>
+        <Info
+          class={'scheme-preview-header-tip'}
+          v-bk-tooltips={{
+            content: '待产品补充',
+          }}
+        />
+        <Select
+          class={'scheme-preivew-header-sort-selector'}
+          v-model={sortChoice.value}
+          clearable={false}
+        >
+          {{
+            default: () => (SchemeSortOptions.map(({ key, val }) => (
+              <Option
+                value={key}
+                label={val}
+              ></Option>
+            ))),
+            suffix: () => (<Button>
+              123
+            </Button>),
+          }}
+        </Select>
+      </div>
+      <div class={'scheme-preview-content'}>
+        123
+      </div>
+    </div>;
+  },
+});
