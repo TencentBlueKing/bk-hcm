426bd77a501dcfbc95591645c624f65cdd74f1fe
coder-cwc <147811460+coder-cwc@users.noreply.github.com>
2023-11-24 11:10:17 +0800

    feat: 完成云选型空白页以及用户占比展示组件的静态页面 (#420)
    * feat: 资源选型空白页第一版样式
    
    * feat: 完成云选型空白页以及用户占比展示组件的静态页面
    
    * style: 调整样式书写位置
    
    * fix: 删除tree静态数据

---
+++ front/src/views/scheme/scheme-recommendation/components/scheme-blank-page/index.tsx
@@ -0,0 +1,35 @@
+import { defineComponent } from 'vue';
+import blank_1 from '@/assets/image/scheme-blank-1.png';
+import blank_2 from '@/assets/image/scheme-blank-2.png';
+import './index.scss';
+
+export default defineComponent({
+  name: 'SchemeBlankPage',
+  setup() {
+    return () => (
+      <div class='blank-show-wrap'>
+        <div class='item-wrap'>
+          <img src={blank_1} alt='' />
+          <div class='title-wrap'>
+            <span class='serial-number mr8'>1</span>
+            <span class='title-text'>配置基本信息</span>
+          </div>
+          <div class='content-wrap'>
+            配置业务基本属性，并查看初步的部署架构、用户分布、部署方案的推荐结果。
+          </div>
+        </div>
+        <i class='hcm-icon bkhcm-icon-arrows-up separator'></i>
+        <div class='item-wrap'>
+          <img src={blank_2} alt='' />
+          <div class='title-wrap'>
+            <span class='serial-number mr8'>2</span>
+            <span class='title-text'>查看方案详情</span>
+          </div>
+          <div class='content-wrap'>
+            查看方案结果，并基于方案分析与网络、成本数据进一步微调部署方案。
+          </div>
+        </div>
+      </div>
+    );
+  },
+});
--- front/src/views/scheme/scheme-recommendation/index.tsx
+++ front/src/views/scheme/scheme-recommendation/index.tsx
@@ -1,11 +1,178 @@
-import { defineComponent } from 'vue';
+import { defineComponent, ref, /* reactive, */ computed } from 'vue';
+import './index.scss';
+import SchemeBlankPage from './components/scheme-blank-page';
+// import { GenerateSchemesReqParams } from './types/index';
+import SchemeUserProportionShowDialog from './components/scheme-user-proportion-show-dialog';
 import SchemePreview from '../components/scheme-preview';
 
 export default defineComponent({
   name: 'SchemeRecommendationPage',
   setup() {
-    return () => <div class='scheme-recommendation-page'>
-      <SchemePreview/>
-    </div>;
+    const toggleClose = ref(false);
+
+    // const countriesList = ref<Array<string>>([]);
+    // const formData = reactive<GenerateSchemesReqParams>({
+    //   biz_type: '',
+    //   cover_ping: null,
+    //   deployment_architecture: [],
+    //   user_distribution: [],
+    // });
+    const formItemOptions = computed(() => [
+      {
+        label: '用户分布地区',
+        required: true,
+        content: () => {
+          return (
+            <bk-select>
+              <bk-option value='1' label='本科以下' />
+              <bk-option value='2' label='本科以上' />
+            </bk-select>
+          );
+        },
+      },
+      {
+        label: '业务类型',
+        required: true,
+        content: () => {
+          return (
+            <bk-select>
+              <bk-option value='1' label='本科以下' />
+              <bk-option value='2' label='本科以上' />
+            </bk-select>
+          );
+        },
+      },
+      {
+        label: '用户网络容忍',
+        extClass: 'prompt-icon-wrap',
+        content: [
+          {
+            label: '网络延迟',
+            content: () => {
+              return <bk-input type='number'></bk-input>;
+            },
+          },
+          {
+            label: 'ping抖动',
+            content: () => {
+              return <bk-input type='number'></bk-input>;
+            },
+          },
+          {
+            label: '丢包率',
+            content: () => {
+              return <bk-input type='number'></bk-input>;
+            },
+          },
+        ],
+      },
+      {
+        label: '用户分布占比',
+        content: () => {
+          return (
+            <div class='flex-row'>
+              <bk-select class='flex-1'>
+                <bk-option value='1' label='默认分布占比' />
+                <bk-option value='2' label='本科以上' />
+              </bk-select>
+              <div class='user-proportion-detail-btn-wrap'>
+                <i class='hcm-icon bkhcm-icon-file'></i>
+                <span
+                  class='btn-text'
+                  onClick={() => {
+                    isUserProportionDetailDialogShow.value = true;
+                  }}>
+                  占比详情
+                </span>
+              </div>
+            </div>
+          );
+        },
+      },
+      {
+        label: '部署架构',
+        extClass: 'prompt-icon-wrap',
+        content: () => {
+          return (
+            <bk-checkbox-group>
+              <bk-checkbox label='分布式部署' />
+              <bk-checkbox label='集中式部署' />
+            </bk-checkbox-group>
+          );
+        },
+      },
+      {
+        label: '部署架构',
+        extClass: 'prompt-icon-wrap',
+        content: () => {
+          return (
+            <>
+              <bk-button class='mr8' theme='primary'>
+                选型推荐
+              </bk-button>
+              <bk-button>清空</bk-button>
+            </>
+          );
+        },
+      },
+    ]);
+    const isUserProportionDetailDialogShow = ref(false);
+    const scene = ref<'blank' | 'preview'>('blank');
+
+    return () => (
+      <>
+        <div class='scheme-recommendation-page'>
+          <div
+            class={`business-attributes-container${
+              toggleClose.value ? ' close' : ''
+            }`}>
+            <div class='title-wrap'>
+              <div class='title-text'>业务属性</div>
+              <i
+                class='hcm-icon bkhcm-icon-shouqi'
+                onClick={() => (toggleClose.value = !toggleClose.value)}></i>
+            </div>
+            <div class='content-wrap'>
+              <bk-form form-type='vertical'>
+                {formItemOptions.value.map(({ label, required, content, extClass }) => {
+                  return (
+                      <bk-form-item
+                        label={label}
+                        required={required}
+                        class={extClass}>
+                        {Array.isArray(content) ? (
+                          <div class='sub-form-item-wrap'>
+                            {content.map((sub) => {
+                              return (
+                                <bk-form-item label={sub.label}>
+                                  {sub.content()}
+                                </bk-form-item>
+                              );
+                            })}
+                          </div>
+                        ) : (
+                          content()
+                        )}
+                      </bk-form-item>
+                  );
+                })}
+              </bk-form>
+            </div>
+          </div>
+          <div class='scheme-recommendation-container'>
+            <div class='content-container'>
+              {scene.value === 'blank' ? (
+                <SchemeBlankPage />
+              ) : (
+                <SchemePreview />
+              )}
+            </div>
+          </div>
+        </div>
+        <SchemeUserProportionShowDialog
+          v-model:isShow={isUserProportionDetailDialogShow.value}
+        />
+      </>
+    );
   },
 });
