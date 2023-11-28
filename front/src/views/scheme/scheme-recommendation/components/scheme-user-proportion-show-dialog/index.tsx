1734d23089ec4d0ba931c9186a0417c57d9f82ba
coder-cwc <147811460+coder-cwc@users.noreply.github.com>
2023-11-28 14:47:07 +0800

    feat: 资源选型空白页接口联调 (#431)

--- front/src/views/scheme/scheme-recommendation/components/scheme-user-proportion-show-dialog/index.tsx
+++ front/src/views/scheme/scheme-recommendation/components/scheme-user-proportion-show-dialog/index.tsx
@@ -1,6 +1,7 @@
-import { PropType, defineComponent, ref } from 'vue';
-import { Dialog, SearchSelect, Tree } from 'bkui-vue';
+import { PropType, defineComponent, ref, watch } from 'vue';
+import { Dialog, Input, Tree } from 'bkui-vue';
 import './index.scss';
+import { IAreaInfo } from '@/typings/scheme';
 
 export default defineComponent({
   name: 'SchemeUserProportionShowDialog',
@@ -9,13 +10,22 @@ export default defineComponent({
       type: Boolean as PropType<boolean>,
       default: false,
     },
+    treeData: {
+      type: Array<IAreaInfo> as PropType<Array<IAreaInfo>>,
+    }
   },
   emits: ['update:isShow'],
   setup(props, ctx) {
     const toggleShow = (isShow: boolean) => {
       ctx.emit('update:isShow', isShow);
     };
-    const treeData = ref([]);
+
+    const searchVal = ref('');
+    const treeData = ref<Array<IAreaInfo>>([]);
+
+    watch(() => props.treeData, (val) => {
+      treeData.value = val;
+    })
 
     return () => (
       <Dialog
@@ -28,21 +38,20 @@ export default defineComponent({
           <i class='hcm-icon bkhcm-icon-info-line'></i>
           <div class='tips-text'>占比权重说明说明说明</div>
         </div>
-        <SearchSelect
+        <Input
+          v-model={searchVal.value}
           class='mb16'
-          modelValue={[]}
-          data={[]}
+          type="search"
           placeholder='请输入'
         />
         <Tree
-          data={treeData.value}
+          data={props.treeData}
           label='name'
           children='children'
-          search=''
+          search={searchVal.value}
           show-node-type-icon={false}
           prefixIcon={(params: any, renderType: any) => {
-            if (params.children.length === 0) return null;
-            console.log(params, renderType);
+            if (params.children?.length === 0) return null;
             return params.isOpen ? (
               <i class='hcm-icon bkhcm-icon-minus-circle'></i>
             ) : (
@@ -50,7 +59,19 @@ export default defineComponent({
             );
           }}>
           {{
-            nodeAppend: () => <span class='proportion-num'>10</span>,
+            nodeAppend: (node: any) => {
+              const {hasChild, isOpen} = node;
+              if (hasChild) {
+                if (isOpen) {
+                  return null;
+                } else {
+                  // 计算子节点的权重之和, 展示到后面
+                  <span class='proportion-num'>{10}</span>
+                }
+              } else {
+                return <span class='proportion-num'>{node.value}</span>
+              }
+           },
           }}
         </Tree>
       </Dialog>
