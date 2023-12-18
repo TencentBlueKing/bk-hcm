27af51c6b797eb27c8cc67ba28ef5eab68bf1d78
coder-cwc <17502088283@163.com>
2023-12-18 09:10:03 +0800

    feat: 管控区绑定增加搜索功能 # Reviewed, transaction id: 587

--- front/src/views/resource/resource-manage/children/manage/vpc-manage.vue
+++ front/src/views/resource/resource-manage/children/manage/vpc-manage.vue
@@ -331,7 +331,7 @@ const renderColumns = [
         {{ curVpc.name || '--' }}
       </bk-form-item>
       <bk-form-item label="管控区名称">
-        <bk-select v-model="curCloudArea">
+        <bk-select v-model="curCloudArea" :input-search="false" filterable>
           <bk-option
             v-for="(item, index) in cloudAreaList"
             :key="index"
