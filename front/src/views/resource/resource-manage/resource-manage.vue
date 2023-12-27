fa35ec0887b2fafc4fb5bce6f79f9ee517984fcd
royalpioneer <royalpioneer@foxmail.com>
2023-12-27 15:06:27 +0800

    feature: 全部账号下支持查看账号信息 # Reviewed, transaction id: 1082

--- front/src/views/resource/resource-manage/resource-manage.vue
+++ front/src/views/resource/resource-manage/resource-manage.vue
@@ -396,10 +396,6 @@ getResourceAccountList();
             :label="item.label"
             :key="item.key"
             :name="item.key"
-            :disabled="
-              item.key === '/resource/resource/account' &&
-                !resourceAccountStore?.resourceAccount?.id
-            "
           />
         </BkTab>
       </div>
