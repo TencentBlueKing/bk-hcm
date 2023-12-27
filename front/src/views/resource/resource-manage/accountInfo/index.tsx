e774ea18a4dc381f7bfd512990664a0d25e5f365
jojo <82189108+royalpioneer@users.noreply.github.com>
2023-12-27 16:30:56 +0800

    feature: 调整账号列表&调整绑定管控区鉴权类型 (#548)
    * fix: 账号管理页下钻详情不能编辑
    
    * feature: 开放原账号录入页
    
    # Reviewed, transaction id: 1074
    
    * feature: 全部账号下支持查看账号信息
    # Reviewed, transaction id: 1082
    
    * feature: 在全部账号下支持查看所有登记账号
    
    # Reviewed, transaction id: 1087
    
    * feature: 调整绑定管控区按钮的权限校验类型
    
    # Reviewed, transaction id: 1090

--- front/src/views/resource/resource-manage/accountInfo/index.tsx
+++ front/src/views/resource/resource-manage/accountInfo/index.tsx
@@ -4,12 +4,14 @@ import { BkTabPanel } from 'bkui-vue/lib/tab';
 import { defineComponent, ref, watch } from 'vue';
 import { RouterView, useRoute, useRouter } from 'vue-router';
 import './index.scss';
+import { useResourceAccountStore } from '@/store/useResourceAccountStore';
 
 export default defineComponent({
   setup() {
     const activeTab = ref(RESOURCE_DETAIL_TABS[0].key);
     const router = useRouter();
     const route = useRoute();
+    const resourceAccountStore = useResourceAccountStore();
     watch(
       () => activeTab.value,
       (val) => {
@@ -25,23 +27,14 @@ export default defineComponent({
     return () => (
       <>
         <div class={'account-info-container'}>
-          <Tab
-            v-model:active={activeTab.value}
-            type='card-grid'>
-               {
-                RESOURCE_DETAIL_TABS.map(({ key, label }) => (
-                  <BkTabPanel
-                    key={key}
-                    label={label}
-                    name={key}
-                  >
-                    <RouterView/>
-                  </BkTabPanel>
-                ))
-               }
+          <Tab v-model:active={activeTab.value} type='card-grid'>
+            {RESOURCE_DETAIL_TABS.filter(({ key }) => resourceAccountStore.resourceAccount?.id || key === '/resource/resource/account/manage').map(({ key, label }) => (
+              <BkTabPanel key={key} label={label} name={key}>
+                <RouterView />
+              </BkTabPanel>
+            ))}
           </Tab>
         </div>
-
       </>
     );
   },
