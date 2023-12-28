ac73812f2414a2eb333dbd4e75a81519cd0a5315
coder-cwc <17502088283@163.com>
2023-12-28 17:52:43 +0800

    feat: 批量删除目标组&批量添加RS # Reviewed, transaction id: 1174

--- front/src/views/business/load-balancer/group-view/index.tsx
+++ front/src/views/business/load-balancer/group-view/index.tsx
@@ -1,5 +1,6 @@
-import { defineComponent, ref } from 'vue';
+import { Ref, defineComponent, ref } from 'vue';
 import { Input, Button, VirtualRender, Dropdown } from 'bkui-vue';
+import { BkRadioGroup, BkRadioButton } from 'bkui-vue/lib/radio';
 import { Plus, AngleDown } from 'bkui-vue/lib/icon';
 import { ISearchItem } from 'bkui-vue/lib/search-select/utils';
 import { useTable } from '@/hooks/useTable/useTable';
@@ -7,6 +8,10 @@ import allIcon from '@/assets/image/all-vendors.png';
 import useColumns from '@/views/resource/resource-manage/hooks/use-columns';
 import CommonSideslider from '@/components/common-sideslider';
 import TargetGroupSidesliderContent from './target-group-sideslider-content';
+import CommonDialog from '@/components/common-dialog';
+import AddRsDialogContent from './add-rs-dialog-content';
+import BatchOperationDialog from '@/components/batch-operation-dialog';
+import RsSidesliderContent from './rs-sideslider-content';
 import './index.scss';
 
 const { BK_HCM_AJAX_URL_PREFIX } = window.PROJECT_CONFIG;
@@ -117,6 +122,7 @@ export default defineComponent({
     });
     const searchValue = ref('');
 
+    // 模拟左侧列表数据
     const renderList = Array(100)
       .fill({})
       .map((_, index: number) => {
@@ -127,9 +133,80 @@ export default defineComponent({
         };
       });
 
+    const currentScene = ref('');
     // 新建目标组 sideslider
-    const isTargetGroupSideslider = ref(false);
-    const handleSubmit = () => {};
+    const isTargetGroupSidesliderShow = ref(false);
+    const isDropdownShow = ref(false);
+    const handleAddTargetGroup = () => {
+      currentScene.value = 'addTargetGroup';
+      isTargetGroupSidesliderShow.value = true;
+      isDropdownShow.value = false;
+    };
+    const handleAddTargetGroupSubmit = () => {};
+
+    // 添加单个RS
+    const isAddRsDialogShow = ref(false);
+    const handleAddRs = () => {
+      if (currentScene.value === 'addTargetGroup') {
+        // todo
+      } else {
+        // todo
+        isBatchAddRsShow.value = true;
+      }
+    };
+
+    // batch operation
+    const isBatchDeleteTargetGroupShow = ref(false);
+    const canDeleteTargetGroup = ref(false);
+    const isBatchDeleteRsShow = ref(false);
+    const isBatchAddRsShow = ref(false);
+    const batchOperationList = [
+      { scene: 'batchDeleteTargetGroup', isShow: isBatchDeleteTargetGroupShow, label: '批量删除目标组' },
+      { scene: 'batchDeleteRs', isShow: isBatchDeleteRsShow, label: '批量移除 RS' },
+      { scene: 'batchAddRs', isShow: isAddRsDialogShow, label: '批量添加 RS' },
+    ];
+    // batch operation item 的点击事件处理函数
+    const handleBatchOperationItemClick = (scene: string, isShow: Ref<boolean>) => {
+      currentScene.value = scene;
+      isDropdownShow.value = false;
+      isShow.value = true;
+    };
+    // 批量删除目标组
+    const batchDeleteTargetGroupTableProps = {
+      data: tableData,
+      columns: [
+        {
+          label: '目标组名称',
+          field: 'target_group_name',
+        },
+        {
+          label: '协议',
+          field: 'protocol',
+          filter: true,
+        },
+        {
+          label: '端口',
+          field: 'port',
+          filter: true,
+        },
+        {
+          label: '关联的负载均衡',
+          field: 'clb_name',
+        },
+        {
+          label: '绑定监听器数量',
+          field: 'listener_count',
+          sort: true,
+          align: 'right',
+        },
+      ],
+      searchData,
+    };
+    const batchDeleteTargetGroup = () => {};
+    // 批量移除 RS
+    const batchDeleteRs = () => {};
+    // 批量添加 RS
+    const handleBatchAddRsSubmit = () => {};
 
     return () => (
       <div class='group-view-page'>
@@ -174,22 +251,24 @@ export default defineComponent({
               {{
                 operation: () => (
                   <>
-                    <Button theme='primary' onClick={() => (isTargetGroupSideslider.value = true)}>
+                    <Button theme='primary' onClick={handleAddTargetGroup}>
                       <Plus class='f20' />
                       新建
                     </Button>
-                    <Dropdown trigger='click' placement='bottom-start'>
+                    <Dropdown trigger='manual' isShow={isDropdownShow.value} placement='bottom-start'>
                       {{
                         default: () => (
-                          <Button>
+                          <Button onClick={() => (isDropdownShow.value = !isDropdownShow.value)}>
                             批量操作 <AngleDown class='f20' />
                           </Button>
                         ),
                         content: () => (
                           <DropdownMenu>
-                            <DropdownItem>批量删除目标组</DropdownItem>
-                            <DropdownItem>批量移除 RS</DropdownItem>
-                            <DropdownItem>批量添加 RS</DropdownItem>
+                            {batchOperationList.map(({ scene, isShow, label }) => (
+                              <DropdownItem onClick={() => handleBatchOperationItemClick(scene, isShow)}>
+                                {label}
+                              </DropdownItem>
+                            ))}
                           </DropdownMenu>
                         ),
                       }}
@@ -203,9 +282,57 @@ export default defineComponent({
         <CommonSideslider
           title='新建目标组'
           width={960}
-          v-model:isShow={isTargetGroupSideslider.value}
-          onHandleSubmit={handleSubmit}>
-          <TargetGroupSidesliderContent />
+          v-model:isShow={isTargetGroupSidesliderShow.value}
+          onHandleSubmit={handleAddTargetGroupSubmit}>
+          <TargetGroupSidesliderContent onShowAddRsDialog={() => (isAddRsDialogShow.value = true)} />
+        </CommonSideslider>
+        <CommonDialog
+          v-model:isShow={isAddRsDialogShow.value}
+          title='添加 RS'
+          width={640}
+          onHandleConfirm={handleAddRs}>
+          <AddRsDialogContent />
+        </CommonDialog>
+        <BatchOperationDialog
+          v-model:isShow={isBatchDeleteTargetGroupShow.value}
+          title='批量删除目标组'
+          theme='danger'
+          confirmText='删除'
+          tableProps={batchDeleteTargetGroupTableProps}
+          onHandleConfirm={batchDeleteTargetGroup}>
+          {{
+            tips: () => (
+              <>
+                已选择 <span class='blue'>150</span> 个目标组，其中可删除 <span class='green'>138</span> 个, 不可删除{' '}
+                <span class='red'>12</span> 个（已绑定了监听器的目标组不可删除）。
+              </>
+            ),
+            tab: () => (
+              <BkRadioGroup v-model={canDeleteTargetGroup.value}>
+                <BkRadioButton label={true}>可删除</BkRadioButton>
+                <BkRadioButton label={false}>不可删除</BkRadioButton>
+              </BkRadioGroup>
+            ),
+          }}
+        </BatchOperationDialog>
+        <BatchOperationDialog
+          v-model:isShow={isBatchDeleteRsShow.value}
+          title='批量移除RS'
+          theme='danger'
+          confirmText='移除 RS'
+          custom
+          onHandleConfirm={batchDeleteRs}>
+          {{}}
+        </BatchOperationDialog>
+        <CommonSideslider
+          title='批量添加 RS'
+          width={960}
+          v-model:isShow={isBatchAddRsShow.value}
+          onHandleSubmit={handleBatchAddRsSubmit}>
+          <RsSidesliderContent
+            selectedTargetGroups={tableData}
+            onShowAddRsDialog={() => (isAddRsDialogShow.value = true)}
+          />
         </CommonSideslider>
       </div>
     );
--- front/src/views/business/load-balancer/group-view/target-group-sideslider-content/index.tsx
+++ front/src/views/business/load-balancer/group-view/target-group-sideslider-content/index.tsx
@@ -1,21 +1,17 @@
-import { computed, defineComponent, reactive, ref } from 'vue';
-import { Form, Select, Input, SearchSelect, Loading, Table } from 'bkui-vue';
+import { computed, defineComponent, reactive } from 'vue';
+import { Form, Select, Input } from 'bkui-vue';
 import { useAccountStore } from '@/store';
-import useColumns from '@/views/resource/resource-manage/hooks/use-columns';
 import AccountSelector from '@/components/account-selector/index.vue';
-import BatchUpdatePopconfirm from '@/components/batch-update-popconfirm';
-import CommonDialog from '@/components/common-dialog';
-import AddRsDialogContent from '../add-rs-dialog-content';
-import Empty from '@/components/empty';
 import './index.scss';
+import RsConfigTable from '../rs-config-table';
 
 const { FormItem } = Form;
 const { Option } = Select;
 
 export default defineComponent({
   name: 'TargetGroupSidesliderContent',
-
-  setup() {
+  emits: ['showAddRsDialog'],
+  setup(_props, { emit }) {
     const accountStore = useAccountStore();
 
     const formData = reactive({
@@ -36,82 +32,6 @@ export default defineComponent({
         formData.bizId = val;
       },
     });
-
-    // rs配置表单项
-    const isTableLoading = ref(false);
-    const { columns, settings } = useColumns('rsConfig');
-    const handleBatchUpdatePort = (_port: number) => {};
-    const handleBatchUpdateWeight = (_weight: number) => {};
-    const handleDeleteRs = () => {};
-    const rsTableColumns = [
-      ...columns,
-      {
-        label: () => (
-          <>
-            <span>端口</span>
-            <BatchUpdatePopconfirm title='端口' onUpdateValue={handleBatchUpdatePort}></BatchUpdatePopconfirm>
-          </>
-        ),
-        field: 'port',
-        isDefaultShow: true,
-      },
-      {
-        label: () => (
-          <>
-            <span>权重</span>
-            <BatchUpdatePopconfirm title='权重' onUpdateValue={handleBatchUpdateWeight}></BatchUpdatePopconfirm>
-          </>
-        ),
-        field: 'weight',
-        isDefaultShow: true,
-      },
-      {
-        label: '',
-        width: 80,
-        render: () => <i class='hcm-icon bkhcm-icon-minus-circle-shape' onClick={handleDeleteRs}></i>,
-      },
-    ];
-    // 补充 port 和 weight 的 settings 配置
-    settings.value.checked.push('port', 'weight');
-    settings.value.fields.push(
-      { label: '端口', field: 'port' },
-      { label: '权重', field: 'weight' },
-    );
-    const rsConfigData = [
-      {
-        privateIp: '10.0.0.1',
-        publicIp: '203.0.113.10',
-        name: '服务器A',
-        region: '华北1区',
-        resourceType: 'VM',
-        network: 'VPC-XYZ',
-        port: 8080,
-        weight: 20,
-      },
-      {
-        privateIp: '10.0.1.2',
-        publicIp: '203.0.113.20',
-        name: '数据库B',
-        region: '华东2区',
-        resourceType: 'RDS',
-        network: 'VPC-ABC',
-        port: 3306,
-        weight: 10,
-      },
-      {
-        privateIp: '10.0.2.3',
-        publicIp: '203.0.113.30',
-        name: '负载均衡C',
-        region: '华南3区',
-        resourceType: 'LoadBalancer',
-        network: 'VPC-DEF',
-        port: 80,
-        weight: 30,
-      },
-    ];
-
-    const isAddRsDialogShow = ref(false);
-    const handleAddRs = () => {};
     const formItemOptions = computed(() => [
       {
         label: '云账号',
@@ -190,29 +110,7 @@ export default defineComponent({
         required: true,
         property: 'rs_list',
         span: 24,
-        content: () => (
-          <>
-            <div class='operation-wrap'>
-              <div class='left-wrap' onClick={() => (isAddRsDialogShow.value = true)}>
-                <i class='hcm-icon bkhcm-icon-plus-circle-shape'></i>
-                <span>添加 RS</span>
-              </div>
-              <div class='search-wrap'>
-                <SearchSelect></SearchSelect>
-              </div>
-            </div>
-            <Loading loading={isTableLoading.value}>
-              <Table data={rsConfigData} columns={rsTableColumns} settings={settings.value} showOverflowTooltip>
-                {{
-                  empty: () => {
-                    if (isTableLoading.value) return null;
-                    return <Empty text='暂未添加实例' />;
-                  },
-                }}
-              </Table>
-            </Loading>
-          </>
-        ),
+        content: () => <RsConfigTable onShowAddRsDialog={() => emit('showAddRsDialog')} />,
       },
     ]);
 
@@ -239,13 +137,6 @@ export default defineComponent({
             </bk-row>
           ))}
         </Form>
-        <CommonDialog
-          v-model:isShow={isAddRsDialogShow.value}
-          title='添加 RS'
-          width={640}
-          onHandleConfirm={handleAddRs}>
-          <AddRsDialogContent />
-        </CommonDialog>
       </bk-container>
     );
   },
