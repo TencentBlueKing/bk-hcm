9f76e2478e5ac774e3ebdb27154bb9f20be3f5e5
coder-cwc <17502088283@163.com>
2024-01-04 15:08:45 +0800

    feat: 添加目标组左侧列表的交互 # Reviewed, transaction id: 1422

--- front/src/views/business/load-balancer/group-view/index.tsx
+++ front/src/views/business/load-balancer/group-view/index.tsx
@@ -1,5 +1,5 @@
 import { defineComponent, ref } from 'vue';
-import { Input, VirtualRender } from 'bkui-vue';
+import { Input, VirtualRender, Popover } from 'bkui-vue';
 import allIcon from '@/assets/image/all-vendors.png';
 import AllGroupsManager from './all-groups-manager';
 import SpecificTargetGroupManager from './specific-target-group-manager';
@@ -29,8 +29,36 @@ export default defineComponent({
       return componentMap[type];
     };
     const activeType = ref('all' as 'all' | 'specific');
-    const handleTypeChange = (type: 'all' | 'specific') => {
+    const highlightItemIndex = ref(-1);
+    const handleTypeChange = (type: 'all' | 'specific', index: number) => {
       activeType.value = type;
+      highlightItemIndex.value = index;
+    };
+
+    const renderDropdownActionList = () => {
+      return (
+        <Popover
+          trigger='click'
+          theme='light'
+          renderType='shown'
+          placement='bottom-start'
+          arrow={false}
+          extCls='more-action-dropdown-menu'>
+          {{
+            default: () => (
+              <div class='more-action'>
+                <i class='hcm-icon bkhcm-icon-more-fill'></i>
+              </div>
+            ),
+            content: () => (
+              <div class='dropdown-action-list'>
+                <div class='dropdown-action-item'>编辑</div>
+                <div class='dropdown-action-item'>删除</div>
+              </div>
+            ),
+          }}
+        </Popover>
+      );
     };
 
     return () => (
@@ -40,27 +68,34 @@ export default defineComponent({
             <Input v-model={searchValue.value} type='search' clearable placeholder='搜索目标组' />
           </div>
           <div class='group-list-wrap'>
-            <div class='all-groups-wrap' onClick={() => handleTypeChange('all')}>
+            <div
+              class={`all-groups-wrap${highlightItemIndex.value === -1 ? ' selected' : ''}`}
+              onClick={() => handleTypeChange('all', -1)}>
               <div class='left-wrap'>
                 <img src={allIcon} alt='' class='prefix-icon' />
                 <span>全部目标组</span>
               </div>
               <div class='right-wrap'>
                 <div class='count'>{6654}</div>
+                {renderDropdownActionList()}
               </div>
             </div>
             <VirtualRender list={renderList} height='calc(100% - 36px)' lineHeight={36}>
               {{
                 default: ({ data }: any) => {
-                  return data.map((item: any) => {
+                  return data.map((item: any, index: number) => {
                     return (
-                      <div key={item.id} class='group-item-wrap' onClick={() => handleTypeChange('specific')}>
+                      <div
+                        key={item.id}
+                        class={`group-item-wrap${highlightItemIndex.value === index ? ' selected' : ''}`}
+                        onClick={() => handleTypeChange('specific', index)}>
                         <div class='left-wrap'>
                           <img src={allIcon} alt='' class='prefix-icon' />
                           <span>{item.name}</span>
                         </div>
                         <div class='right-wrap'>
                           <div class='count'>{item.count}</div>
+                          {renderDropdownActionList()}
                         </div>
                       </div>
                     );
