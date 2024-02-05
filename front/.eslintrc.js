157fc1ef985c28e01eff0d61c3155bad16f71bd5
coder-cwc <17502088283@163.com>
2024-02-05 09:24:38 +0800

    lint: 添加no-console, no-debugger规则 # Reviewed, transaction id: 2655

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -12,5 +12,7 @@ module.exports = {
     '@typescript-eslint/naming-convention': 0,
     '@typescript-eslint/no-misused-promises': 0,
     'prefer-spread': 'off',
+    'no-console': 'error',
+    'no-debugger': 'error',
   },
 };
