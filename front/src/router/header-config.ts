eb4583daad018984fd2a7f7626f130ce73c22728
coder-cwc <17502088283@163.com>
2024-02-28 17:32:55 +0800

    fix: 修复业务选择器相关的业务
    - 修复: 页面刷新后, 业务选择器只会选择第一个业务
    - 完善: url 中携带 bizs 参数
    # Reviewed, transaction id: 3078

--- front/src/router/header-config.ts
+++ front/src/router/header-config.ts
@@ -6,26 +6,22 @@ export const headRouteConfig = [
   {
     id: 'business',
     name: '资源管理',
-    route: 'business',
-    href: '#/business/host',
+    path: '/business/host',
   },
   {
     id: 'service',
     name: '我的单据',
-    route: 'service',
-    href: '#/service/my-apply',
+    path: '/service/my-apply',
   },
   {
     id: 'resource',
     name: '资源接入',
-    route: 'resource',
-    href: '#/resource/resource',
+    path: '/resource/resource',
   },
   {
     id: 'scheme',
     name: '资源选型',
-    route: 'scheme',
-    href: '#/scheme/recommendation',
+    path: '/scheme/recommendation',
   },
 
   // 接下来是 资源选型、平台管理
