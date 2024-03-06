700398f9364b91475a92ccb72a05dc65d300237e
coder-cwc <17502088283@163.com>
2024-03-06 15:27:15 +0800

    fix(v1.4.x): 解决合并错误 # Reviewed, transaction id: 3408

--- front/src/store/resource.ts
+++ front/src/store/resource.ts
@@ -229,9 +229,5 @@ export const useResourceStore = defineStore({
     create(type: string, data: any) {
       return http.post(`${BK_HCM_AJAX_URL_PREFIX}/api/v1/cloud/${getBusinessApiPath(type)}${type}/create`, data);
     },
-    // 创建
-    create(type: string, data: any) {
-      return http.post(`${BK_HCM_AJAX_URL_PREFIX}/api/v1/cloud/${getBusinessApiPath(type)}${type}/create`, data);
-    },
   },
 });
