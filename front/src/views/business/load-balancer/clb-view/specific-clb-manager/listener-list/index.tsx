1e75ce0d2cb6bd51c17705496bdfd81a985c7f9c
royalpioneer <royalpioneer@foxmail.com>
2024-03-15 16:25:48 +0800

    feature: 新增监听器 # Reviewed, transaction id: 3950

--- front/src/views/business/load-balancer/clb-view/specific-clb-manager/listener-list/index.tsx
+++ front/src/views/business/load-balancer/clb-view/specific-clb-manager/listener-list/index.tsx
@@ -1,8 +1,13 @@
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import './index.scss';
 import { useTable } from '@/hooks/useTable/useTable';
-import { Button } from 'bkui-vue';
+import { Button, Form, Input, Radio, Select, Switcher, Tag } from 'bkui-vue';
 import { Plus } from 'bkui-vue/lib/icon';
+import CommonSideslider from '@/components/common-sideslider';
+import { BkButtonGroup } from 'bkui-vue/lib/button';
+import { BkRadioGroup } from 'bkui-vue/lib/radio';
+
+const { FormItem } = Form;
 
 export default defineComponent({
   setup() {
@@ -45,6 +50,12 @@ export default defineComponent({
       },
       tableOptions: {
         columns: [
+          {
+            type: 'selection',
+            width: 32,
+            minWidth: 32,
+            align: 'right',
+          },
           {
             label: '监听器名称',
             field: 'listenerName',
@@ -125,13 +136,14 @@ export default defineComponent({
         type: '',
       },
     });
+    const isSliderShow = ref(false);
     return () => (
       <div>
         <CommonTable>
           {{
             operation: () => (
               <div class={'flex-row align-item-center'}>
-                <Button theme={'primary'}>
+                <Button theme={'primary'} onClick={() => (isSliderShow.value = true)}>
                   <Plus class={'f20'} />
                   新增监听器
                 </Button>
@@ -140,6 +152,71 @@ export default defineComponent({
             ),
           }}
         </CommonTable>
+        <CommonSideslider
+          v-model:isShow={isSliderShow.value}
+          title={'新增监听器'}
+          width={640}
+          onHandleSubmit={() => {}}>
+          <Form formType='vertical'>
+            <FormItem label='监听器名称' required>
+              <Input placeholder='请输入' />
+            </FormItem>
+            <FormItem label='监听协议' required>
+              <BkButtonGroup>
+                <Button>TCP</Button>
+                <Button>UDP</Button>
+                <Button>HTTP</Button>
+                <Button>HTTPS</Button>
+              </BkButtonGroup>
+            </FormItem>
+            <FormItem label='监听端口' required>
+              <Input placeholder='请输入' />
+            </FormItem>
+
+            <div class={'flex-row justify-content-between'}>
+              <FormItem label='SNI' required>
+                <Switcher />
+              </FormItem>
+              <FormItem label='SSL解析方式' required>
+                <BkRadioGroup>
+                  <Radio label='单向认证'></Radio>
+                  <Tag theme='info'>推荐</Tag>
+                  <Radio label='双向认证' class={'ml24'}></Radio>
+                </BkRadioGroup>
+              </FormItem>
+            </div>
+            <FormItem label='服务器证书' required>
+              <Select></Select>
+            </FormItem>
+            <FormItem label='CA证书' required>
+              <Select></Select>
+            </FormItem>
+            <FormItem label='默认域名' required>
+              <Input placeholder='请输入' />
+            </FormItem>
+            <FormItem label='URL路径' required>
+              <Input placeholder='请输入' />
+            </FormItem>
+
+            <FormItem label='均衡方式' required>
+              <Select></Select>
+            </FormItem>
+            <FormItem label='监听器名称' required>
+              <Input placeholder='请输入' />
+            </FormItem>
+            <div class={'flex-row'}>
+              <FormItem label='会话保持' required>
+                <Switcher />
+              </FormItem>
+              <FormItem label='保持时间' class={'ml24'} required>
+                <Input placeholder='请输入' type='number' suffix='秒' />
+              </FormItem>
+            </div>
+            <FormItem label='目标组' required>
+              <Select></Select>
+            </FormItem>
+          </Form>
+        </CommonSideslider>
       </div>
     );
   },
