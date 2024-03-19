a42bb6a219c4b42118fc51d8de690f5809c8d4f3
chenjr15 <chenjr15@live.cn>
2024-03-19 11:45:11 +0800

    feat: 监听器下域名列表 url和method改为list/post
    --story=116326117 域名-监听器下域名列表

--- cmd/cloud-server/service/load-balancer/load_balancer.go
+++ cmd/cloud-server/service/load-balancer/load_balancer.go
@@ -66,8 +66,8 @@ func InitService(c *capability.Capability) {
 	bizH.Add("ListBizTCloudRuleByTG", http.MethodPost,
 		"/vendors/tcloud/target_groups/{target_group_id}/rules/list", svc.ListBizTCloudRuleByTG)
 	bizH.Add("GetBizListener", http.MethodGet, "/listeners/{id}", svc.GetBizListener)
-	bizH.Add("GetBizListenerDomains", http.MethodGet,
-		"/vendors/tcloud/listeners/{lbl_id}/domains", svc.GetBizListenerDomains)
+	bizH.Add("ListBizListenerDomains", http.MethodPost,
+		"/vendors/tcloud/listeners/{lbl_id}/domains/list", svc.ListBizListenerDomains)
 	bizH.Add("GetBizTCloudUrlRule", http.MethodGet,
 		"/vendors/tcloud/listeners/{lbl_id}/rules/{rule_id}", svc.GetBizTCloudUrlRule)
 
