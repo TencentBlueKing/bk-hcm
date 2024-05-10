b60245590aa64c5ce6637295dd35e0ee6bcb8e53
coder-cwc <17502088283@163.com>
2024-05-10 12:27:44 +0800

    fix(lb-tree): 修复负载均衡拓扑结构问题
    --bug=123786875
    --bug=123786963
    --bug=123786795
    --bug=123787005
    --bug=123787015
    # Reviewed, transaction id: 7474

--- front/src/views/business/load-balancer/clb-view/index.tsx
+++ front/src/views/business/load-balancer/clb-view/index.tsx
@@ -1,6 +1,7 @@
-import { computed, defineComponent, ref } from 'vue';
+import { computed, defineComponent } from 'vue';
 import { RouterView, useRoute } from 'vue-router';
 // import components
+import { ResizeLayout } from 'bkui-vue';
 import LbTree from './lb-tree';
 import LbBreadcrumb from '../components/lb-breadcrumb';
 import './index.scss';
@@ -10,8 +11,6 @@ export default defineComponent({
   setup() {
     const route = useRoute();
 
-    const isAdvancedSearchShow = ref(false);
-
     // 面包屑白名单
     const BREADCRUMB_WHITE_LIST = ['listener', 'domain'];
     const isBreadcrumbShow = computed(() => {
@@ -19,17 +18,22 @@ export default defineComponent({
     });
 
     return () => (
-      <div class='clb-view-page'>
-        <div class='left-container'>
-          <LbTree />
-        </div>
-        {isAdvancedSearchShow.value && <div class='advanced-search'>高级搜索</div>}
-        <div class='main-container'>
-          {isBreadcrumbShow.value && <LbBreadcrumb />}
-          {/* 四级路由 */}
-          <RouterView />
-        </div>
-      </div>
+      <ResizeLayout class='clb-view-page' collapsible initialDivide={300}>
+        {{
+          aside: () => (
+            <div class='left-container'>
+              <LbTree />
+            </div>
+          ),
+          main: () => (
+            <div class='main-container'>
+              {isBreadcrumbShow.value && <LbBreadcrumb />}
+              {/* 四级路由 */}
+              <RouterView />
+            </div>
+          ),
+        }}
+      </ResizeLayout>
     );
   },
 });
