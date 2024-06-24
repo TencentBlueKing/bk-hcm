5485027d06b9e2a5f637364e2b6e7cce70ac67fe
chenjr15 <chenjr15@live.cn>
2024-06-24 09:45:29 +0800

    fix: add log

--- cmd/account-server/service/bill/billadjustment/bill_adjustment.go
+++ cmd/account-server/service/bill/billadjustment/bill_adjustment.go
@@ -185,10 +185,14 @@ func (b *billAdjustmentSvc) ListBillAdjustmentItem(cts *rest.Contexts) (any, err
 	// 填充主账号云id 和 email
 	for i, adjustmentItem := range resp.Details {
 		mainAccount := mainAccIDCloudIDMap[adjustmentItem.MainAccountID]
-		if mainAccount != nil {
-			resp.Details[i].MainAccountCloudID = mainAccIDCloudIDMap[adjustmentItem.MainAccountID].CloudID
-			resp.Details[i].MainAccountEmail = mainAccIDCloudIDMap[adjustmentItem.MainAccountID].Email
+		if mainAccount == nil {
+			// Skip not found account info
+			logs.Warnf("main account of bill adjustment not found, main account id: %s, adjustment id: %s, rid: %s",
+				adjustmentItem.MainAccountID, adjustmentItem.ID, cts.Kit.Rid)
+			continue
 		}
+		resp.Details[i].MainAccountCloudID = mainAccount.CloudID
+		resp.Details[i].MainAccountEmail = mainAccount.Email
 	}
 	return resp, nil
 }
