703d28d7bdadb53bc15b7d7056981a82e07ff503
royalpioneer <royalpioneer@foxmail.com>
2024-06-26 14:21:05 +0800

    feat: 修复账号问题
    --story=118158672

--- front/src/views/bill/account/account-manage/constants.ts
+++ front/src/views/bill/account/account-manage/constants.ts
@@ -1,4 +1,4 @@
-import { convertToIdNameMap } from "./util";
+import { convertToIdNameMap } from './util';
 
 export enum AccountLevelEnum {
   FirstLevel = 'firstAccount',
@@ -14,39 +14,6 @@ export const tabs = [
   {
     key: AccountLevelEnum.SecondLevel,
     label: '二级账号',
-  },
-];
-
-export const reviewData = [
-  {
-    primaryAccountName: '主账户A',
-    primaryAccountId: '12345',
-    cloudProvider: 'AWS',
-    accountEmail: 'exampleA@example.com',
-    mainResponsiblePerson: '张三',
-    organizationalStructure: '部门A',
-    secondaryAccountCount: 5,
-    actions: 'Delete',
-  },
-  {
-    primaryAccountName: '主账户B',
-    primaryAccountId: '67890',
-    cloudProvider: 'Azure',
-    accountEmail: 'exampleB@example.com',
-    mainResponsiblePerson: '李四',
-    organizationalStructure: '部门B',
-    secondaryAccountCount: 3,
-    actions: 'Update',
-  },
-  {
-    primaryAccountName: '主账户C',
-    primaryAccountId: '54321',
-    cloudProvider: 'Google Cloud',
-    accountEmail: 'exampleC@example.com',
-    mainResponsiblePerson: '王五',
-    organizationalStructure: '部门C',
-    secondaryAccountCount: 7,
-    actions: 'View',
   },
 ];
 
@@ -104,52 +71,15 @@ export const searchData = [
   },
 ];
 
-// 二级账号
-export const secondaryReviewData = [
-  {
-    secondaryAccountName: '子账户A',
-    secondaryAccountId: '10001',
-    parentPrimaryAccount: '主账户A',
-    cloudProvider: 'AWS',
-    siteType: '电商',
-    accountEmail: 'childA@example.com',
-    mainResponsiblePerson: '刘一',
-    operatingProduct: '产品A',
-    actions: 'Delete',
-  },
-  {
-    secondaryAccountName: '子账户B',
-    secondaryAccountId: '10002',
-    parentPrimaryAccount: '主账户B',
-    cloudProvider: 'Azure',
-    siteType: '社交',
-    accountEmail: 'childB@example.com',
-    mainResponsiblePerson: '陈二',
-    operatingProduct: '产品B',
-    actions: 'Update',
-  },
-  {
-    secondaryAccountName: '子账户C',
-    secondaryAccountId: '10003',
-    parentPrimaryAccount: '主账户C',
-    cloudProvider: 'Google Cloud',
-    siteType: '媒体',
-    accountEmail: 'childC@example.com',
-    mainResponsiblePerson: '张三',
-    operatingProduct: '产品C',
-    actions: 'View',
-  },
-];
-
 export const BILL_SITE_TYPES = [
-    {
-        name: '中国站',
-        id: 'china',
-      },
-      {
-        name: '国际站',
-        id: 'international',
-      },
+  {
+    name: '中国站',
+    id: 'china',
+  },
+  {
+    name: '国际站',
+    id: 'international',
+  },
 ];
 
 export const BILL_SITE_TYPES_MAP = convertToIdNameMap(BILL_SITE_TYPES);
