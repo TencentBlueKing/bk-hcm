1fa3169f412ed0997125ad8eee16ea6660add25e
coder-cwc <147811460+coder-cwc@users.noreply.github.com>
2024-07-12 17:19:28 +0800

    feat: 账号管理-账单汇率显示 (#947)
    --story=118227560
    # Reviewed, transaction id: 11918

--- front/src/views/bill/bill/header/index.tsx
+++ front/src/views/bill/bill/header/index.tsx
@@ -1,9 +1,11 @@
-import { Ref, defineComponent, inject, ref } from 'vue';
+import { Ref, defineComponent, inject, ref, watch } from 'vue';
 import { DatePicker } from 'bkui-vue';
 import { RouterLink, useRoute } from 'vue-router';
 import './index.scss';
 import { useI18n } from 'vue-i18n';
 import { BILL_BIZS_KEY, BILL_MAIN_ACCOUNTS_KEY } from '@/constants';
+import { reqBillsExchangeRateList } from '@/api/bill';
+import { QueryRuleOPEnum } from '@/typings';
 
 export default defineComponent({
   setup() {
@@ -11,6 +13,8 @@ export default defineComponent({
     const { t } = useI18n();
 
     const currentMonth = inject<Ref<Date>>('currentMonth');
+    const bill_year = inject<Ref<number>>('bill_year');
+    const bill_month = inject<Ref<number>>('bill_month');
 
     const links = ref([
       { name: 'billSummary', title: t('账单汇总') },
@@ -18,11 +22,46 @@ export default defineComponent({
       { name: 'billAdjust', title: t('账单调整') },
     ]);
 
+    const currentMonthExchangeRate = ref('');
+
+    watch(
+      currentMonth,
+      async () => {
+        const res = await reqBillsExchangeRateList({
+          filter: {
+            op: QueryRuleOPEnum.AND,
+            rules: [
+              { field: 'year', op: QueryRuleOPEnum.EQ, value: bill_year.value },
+              { field: 'month', op: QueryRuleOPEnum.EQ, value: bill_month.value },
+              { field: 'from_currency', op: QueryRuleOPEnum.EQ, value: 'USD' },
+              { field: 'to_currency', op: QueryRuleOPEnum.EQ, value: 'CNY' },
+            ],
+          },
+          page: { start: 0, limit: 10, count: false },
+        });
+        currentMonthExchangeRate.value = res.data?.details[0]?.exchange_rate;
+      },
+      { immediate: true },
+    );
+
     return () => (
       <div class='header-container'>
         <div class='title-wrap'>
           <div class='title'>{t('云账单管理')}</div>
           <DatePicker v-model={currentMonth.value} type='month' clearable={false} />
+          <div class='rate-wrapper'>
+            <span
+              class='rate-label'
+              v-bk-tooltips={{ content: t('当月汇率每月为固定值，由平台统一提供汇率，非实时汇率值。') }}>
+              {t('当月汇率')}
+            </span>
+            ：
+            <span class='rate-value'>
+              {currentMonthExchangeRate.value
+                ? `1$(${t('美金')})=${currentMonthExchangeRate.value}￥(${t('人民币')})`
+                : t('本月汇率未提供')}
+            </span>
+          </div>
         </div>
         <div class='link-wrap'>
           {links.value.map(({ name, title }) => (
