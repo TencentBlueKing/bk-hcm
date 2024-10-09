aa40dff0a90d77392186d3e3e3f6f936c96a638e
coder-cwc <17502088283@163.com>
2024-10-09 10:08:58 +0800

    refactor: 重构负载均衡树
    --story=119574807

---
+++ front/src/views/business/load-balancer/clb-view/lb-tree/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useTemplateRef } from 'vue';
+import ITree from './tree/index.vue';
+import ISearch from './search/index.vue';
+import { RulesItem } from '@/typings';
+
+const treeRef = useTemplateRef<typeof ITree>('tree');
+const handleSearch = (rules: RulesItem[], searchValue: string) => {
+  treeRef.value.search(rules, searchValue);
+};
+</script>
+
+<template>
+  <ISearch @search="handleSearch" />
+  <ITree ref="tree" />
+</template>
+
+<style scoped></style>
