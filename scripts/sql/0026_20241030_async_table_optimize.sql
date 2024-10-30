3a360256ae259884f3059c7eadfce26859079ea9
chenjr15 <24223746+chenjr15@users.noreply.github.com>
2024-10-30 19:44:53 +0800

    docs: v1.6.10 release (#1144)

--- scripts/sql/0026_20241030_async_table_optimize.sql
+++ scripts/sql/0026_20241030_async_table_optimize.sql
@@ -24,6 +24,7 @@
     1. 修改`async_flow`表，增加`worker`,`state`索引
     2. 修改`async_flow`表，增加`state`索引
     3. 修改`account_bill_item`表，增加(`root_account_id`, `main_account_id`, `bill_day`)索引
+    4. 修改`account_bill_sync_record`表，增加`adjustment_flow_id`字段
 */
 
 START TRANSACTION;
@@ -37,6 +38,8 @@ alter table account_bill_item
     add index `idx_root_main_bill_day` (`root_account_id`, `main_account_id`, `bill_day`);
 
 
+alter table account_bill_sync_record
+    add adjustment_flow_id varchar(64) not null default '' after detail;
 
 CREATE OR REPLACE VIEW `hcm_version`(`hcm_ver`, `sql_ver`) AS
 SELECT 'v1.6.10' as `hcm_ver`, '0026' as `sql_ver`;
