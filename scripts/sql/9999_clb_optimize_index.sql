4361e0e431d51d661c679a22dcbd65ac4c3c9c7c
chenjr15 <chenjr15@live.cn>
2024-11-05 09:59:55 +0800

    reformat: sql

--- scripts/sql/9999_clb_optimize_index.sql
+++ scripts/sql/9999_clb_optimize_index.sql
@@ -27,26 +27,37 @@
 
 START TRANSACTION;
 
-alter table tcloud_lb_url_rule add index idx_cloud_lbl_id_rule_type(cloud_lbl_id,rule_type);
-alter table tcloud_lb_url_rule add index idx_lb_id_rule_type(lb_id,rule_type);
+-- tcloud_lb_url_rule
+alter table tcloud_lb_url_rule
+    add index idx_cloud_lbl_id_rule_type (cloud_lbl_id, rule_type);
+alter table tcloud_lb_url_rule
+    add index idx_lb_id_rule_type (lb_id, rule_type);
 
-alter table load_balancer_target add index idx_target_group_id (target_group_id);
+-- load_balancer_target
+alter table load_balancer_target
+    add index idx_target_group_id (target_group_id);
 
-alter table target_group_listener_rule_rel add index idx_listener_rule_id(listener_rule_id);
-alter table target_group_listener_rule_rel add index idx_lb_id_cloud_rule_id(lb_id,cloud_listener_rule_id);
-alter table target_group_listener_rule_rel add index idx_lb_id_cloud_lbl_id(lb_id,cloud_lbl_id);
-alter table target_group_listener_rule_rel add index idx_lbl_id(lbl_id);
+-- target_group_listener_rule_rel
+alter table target_group_listener_rule_rel
+    add index idx_listener_rule_id (listener_rule_id);
+alter table target_group_listener_rule_rel
+    add index idx_lb_id_cloud_rule_id (lb_id, cloud_listener_rule_id);
+alter table target_group_listener_rule_rel
+    add index idx_lb_id_cloud_lbl_id (lb_id, cloud_lbl_id);
+alter table target_group_listener_rule_rel
+    add index idx_lbl_id (lbl_id);
 
+-- load_balancer_listener
+alter table load_balancer_listener
+    add index idx_vendor_account_id_bk_biz_id_cloud_lb_id (vendor, account_id, bk_biz_id, cloud_lb_id);
 
-alter table load_balancer_listener add index idx_vendor_account_id_bk_biz_id_cloud_lb_id(vendor,account_id,bk_biz_id,cloud_lb_id);
+-- load_balancer_target_group
+alter table load_balancer_target_group
+    add index idx_bk_biz_id (bk_biz_id);
 
-alter table load_balancer_target_group add index idx_bk_biz_id(bk_biz_id);
-
-
-alter table audit add index idx_bk_biz_id(bk_biz_id);
-
-
-
+-- audit
+alter table audit
+    add index idx_bk_biz_id (bk_biz_id);
 
 CREATE OR REPLACE VIEW `hcm_version`(`hcm_ver`, `sql_ver`) AS
 SELECT 'v9.9.9' as `hcm_ver`, '9999' as `sql_ver`;
