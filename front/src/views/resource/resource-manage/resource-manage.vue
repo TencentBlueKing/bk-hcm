5281494c7770010d158856322b2bd7b84d4e2f69
chen.zhang <422734124@qq.com>
2024-11-20 15:30:00 +0800

    fix: 资源接入-安全组建没有自动加载左侧账号 --story=133888747

--- front/src/views/resource/resource-manage/resource-manage.vue
+++ front/src/views/resource/resource-manage/resource-manage.vue
@@ -558,6 +558,7 @@ onMounted(() => {
             @success="handleSuccess"
             :is-edit="isEdit"
             :detail="formDetail"
+            :show="isShowSideSlider"
             @edit="handleEdit"
             v-model:isFormDataChanged="isFormDataChanged"
           ></component>
