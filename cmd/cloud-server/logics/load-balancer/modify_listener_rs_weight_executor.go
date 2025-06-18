7cfb376ccd93a1ac2d2fd19f9c57c7c075d062e0
ironguo <783332991@qq.com>
2025-06-18 17:43:22 +0800

    feat: 负载均衡-解绑RS、权重调整，拆分为4层/7层 --story=125014679

--- cmd/cloud-server/logics/load-balancer/modify_listener_rs_weight_executor.go
+++ cmd/cloud-server/logics/load-balancer/modify_listener_rs_weight_executor.go
@@ -45,10 +45,12 @@ import (
 )
 
 func newBatchListenerModifyRsWeightExecutor(cli *dataservice.Client, taskCli *taskserver.Client,
-	vendor enumor.Vendor, bkBizID int64, accountID string, regionIDs []string) *BatchListenerModifyRsWeightExecutor {
+	vendor enumor.Vendor, bkBizID int64, accountID string, regionIDs []string,
+	operationType OperationType) *BatchListenerModifyRsWeightExecutor {
 
 	return &BatchListenerModifyRsWeightExecutor{
 		taskType:            enumor.ListenerModifyRsWeightTaskType,
+		operationType:       enumor.TaskOperation(operationType),
 		taskCli:             taskCli,
 		basePreviewExecutor: newBasePreviewExecutor(cli, vendor, bkBizID, accountID, regionIDs),
 	}
@@ -58,11 +60,12 @@ func newBatchListenerModifyRsWeightExecutor(cli *dataservice.Client, taskCli *ta
 type BatchListenerModifyRsWeightExecutor struct {
 	*basePreviewExecutor
 
-	taskType    enumor.TaskType
-	taskCli     *taskserver.Client
-	params      *dataproto.ListListenerWithTargetsReq
-	details     []*dataproto.ListBatchListenerResult
-	taskDetails []*batchListenerModifyRsWeightTaskDetail
+	taskType      enumor.TaskType
+	operationType enumor.TaskOperation
+	taskCli       *taskserver.Client
+	params        *dataproto.ListListenerWithTargetsReq
+	details       []*dataproto.ListBatchListenerResult
+	taskDetails   []*batchListenerModifyRsWeightTaskDetail
 }
 
 // 用于记录 detail - 异步任务flow&task - 任务管理 之间的关系
@@ -128,23 +131,27 @@ func (c *BatchListenerModifyRsWeightExecutor) getNewListenerRsList(kt *kit.Kit) 
 	// 检查RS的最新权重是否已更新
 	newLblRsList := make([]*dataproto.ListBatchListenerResult, 0)
 	for _, item := range lblResp.Details {
+		tmpLblRs := &dataproto.ListBatchListenerResult{
+			ClbID:        item.ClbID,
+			CloudClbID:   item.CloudClbID,
+			ClbVipDomain: item.ClbVipDomain,
+			BkBizID:      item.BkBizID,
+			Region:       item.Region,
+			Vendor:       item.Vendor,
+			LblID:        item.LblID,
+			CloudLblID:   item.CloudLblID,
+			Protocol:     item.Protocol,
+			Port:         item.Port,
+			NewRsWeight:  c.params.NewRsWeight,
+		}
+
 		for _, rsItem := range item.RsList {
 			if cvt.PtrToVal(rsItem.Weight) != cvt.PtrToVal(c.params.NewRsWeight) {
-				newLblRsList = append(newLblRsList, &dataproto.ListBatchListenerResult{
-					ClbID:        item.ClbID,
-					CloudClbID:   item.CloudClbID,
-					ClbVipDomain: item.ClbVipDomain,
-					BkBizID:      item.BkBizID,
-					Region:       item.Region,
-					Vendor:       item.Vendor,
-					LblID:        item.LblID,
-					CloudLblID:   item.CloudLblID,
-					Protocol:     item.Protocol,
-					Port:         item.Port,
-					RsList:       item.RsList,
-					NewRsWeight:  c.params.NewRsWeight,
-				})
+				tmpLblRs.RsList = append(tmpLblRs.RsList, rsItem)
 			}
+		}
+		if len(tmpLblRs.RsList) > 0 {
+			newLblRsList = append(newLblRsList, tmpLblRs)
 		}
 	}
 	return newLblRsList, nil
@@ -277,7 +284,7 @@ func (c *BatchListenerModifyRsWeightExecutor) createTaskManagement(
 				AccountIDs: []string{c.accountID},
 				Resource:   enumor.TaskManagementResClb,
 				State:      enumor.TaskManagementRunning, // 默认:执行中
-				Operations: []enumor.TaskOperation{enumor.TaskModifyListenerRsWeight},
+				Operations: []enumor.TaskOperation{c.operationType},
 				Extension: &coretask.ManagementExt{
 					LblTargetsReq: c.params,
 				},
@@ -305,7 +312,7 @@ func (c *BatchListenerModifyRsWeightExecutor) createTaskDetails(kt *kit.Kit, tas
 		taskDetailsCreateReq.Items = append(taskDetailsCreateReq.Items, task.CreateDetailField{
 			BkBizID:          c.bkBizID,
 			TaskManagementID: taskID,
-			Operation:        enumor.TaskModifyListenerRsWeight,
+			Operation:        c.operationType,
 			State:            enumor.TaskDetailInit,
 			Param:            detail,
 		})
@@ -319,7 +326,7 @@ func (c *BatchListenerModifyRsWeightExecutor) createTaskDetails(kt *kit.Kit, tas
 
 	if len(result.IDs) != len(c.details) {
 		return fmt.Errorf("create task details failed, operation: %s, expect created[%d] task details, but got [%d]",
-			enumor.TaskModifyListenerRsWeight, len(c.details), len(result.IDs))
+			c.operationType, len(c.details), len(result.IDs))
 	}
 
 	for i := range result.IDs {
