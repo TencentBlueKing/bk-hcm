0fc631cb0dc6d447dacc9847801e2af4238aaccd
coder-cwc <17502088283@163.com>
2025-06-19 17:05:24 +0800

    feat: 安全组-同步改为同步方式 --story=124966509 # Reviewed, transaction id: 47689

--- front/src/store/resource.ts
+++ front/src/store/resource.ts
@@ -291,10 +291,11 @@ export const useResourceStore = defineStore({
       return http.post(`${BK_HCM_AJAX_URL_PREFIX}/api/v1/cloud/${getBusinessApiPath(type)}${type}/create`, data);
     },
     // 同步拉取资源
-    syncResource(vendor: string, accountId: string, resourceName: string, params: SyncResourceParams) {
+    syncResource(vendor: string, accountId: string, resourceName: string, params: SyncResourceParams, config?: any) {
       return http.post(
         `/api/v1/cloud/${getBusinessApiPath()}vendors/${vendor}/accounts/${accountId}/resources/${resourceName}/sync_by_cond`,
         params,
+        config,
       );
     },
   },
