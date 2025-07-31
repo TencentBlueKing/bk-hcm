49982e78cc5885ab284d0dc7d696d1c510c372f3
kookouse <kookouse@gmail.com>
2025-07-31 10:18:48 +0800

    feat: 登记账号-名称长度调整 --story=126252708

---
+++ scripts/sql/9999_account.sql
@@ -0,0 +1,35 @@
+/*
+ * TencentBlueKing is pleased to support the open source community by making
+ * 蓝鲸智云 - 混合云管理平台 (BlueKing - Hybrid Cloud Management System) available.
+ * Copyright (C) 2024 THL A29 Limited,
+ * a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * We undertake not to change the open source license (MIT license) applicable
+ *
+ * to the current version of the project delivered to anyone in the future.
+ */
+
+/*
+    SQLVER=9999,HCMVER=v9.9.9
+
+    Notes:
+    1. 修改`account`表，修改`name`字段长度
+*/
+
+START TRANSACTION;
+
+ALTER TABLE `account`
+    modify name VARCHAR(255) NOT NULL;
+
+CREATE OR REPLACE VIEW `hcm_version`(`hcm_ver`, `sql_ver`) AS
+SELECT 'v9.9.9' as `hcm_ver`, '9999' as `sql_ver`;
+
+COMMIT;
