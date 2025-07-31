7267aa94ef209b0a97e5316edad7fd47fb4aaa1b
KooKouse <kookouse@gmail.com>
2025-07-31 15:05:38 +0800

    feat: 登记账号-名称长度调整 --story=126252708 (#1661)

--- pkg/api/data-service/cloud/account.go
+++ pkg/api/data-service/cloud/account.go
@@ -35,7 +35,7 @@ import (
 // AccountExtensionCreateReq account extension create req.
 type AccountExtensionCreateReq interface {
 	TCloudAccountExtensionCreateReq | AwsAccountExtensionCreateReq | HuaWeiAccountExtensionCreateReq |
-	GcpAccountExtensionCreateReq | AzureAccountExtensionCreateReq | OtherAccountExtensionCreateReq
+		GcpAccountExtensionCreateReq | AzureAccountExtensionCreateReq | OtherAccountExtensionCreateReq
 }
 
 // OtherAccountExtensionCreateReq ...
@@ -146,7 +146,7 @@ func (c *AccountCreateReq[T]) Validate() error {
 // Note: 对于允许为空字符串的字段，则其类型需要定义为指针，正常情况下，Json合并时空值会被忽略
 type AccountExtensionUpdateReq interface {
 	TCloudAccountExtensionUpdateReq | AwsAccountExtensionUpdateReq | HuaWeiAccountExtensionUpdateReq |
-	GcpAccountExtensionUpdateReq | AzureAccountExtensionUpdateReq | OtherAccountExtensionUpdateReq
+		GcpAccountExtensionUpdateReq | AzureAccountExtensionUpdateReq | OtherAccountExtensionUpdateReq
 }
 
 // TCloudAccountExtensionUpdateReq ...
@@ -270,7 +270,7 @@ func (u *AccountUpdateReq[T]) Validate() error {
 // AccountExtensionGetResp ...
 type AccountExtensionGetResp interface {
 	cloud.TCloudAccountExtension | cloud.AwsAccountExtension | cloud.HuaWeiAccountExtension |
-	cloud.GcpAccountExtension | cloud.AzureAccountExtension | cloud.OtherAccountExtension
+		cloud.GcpAccountExtension | cloud.AzureAccountExtension | cloud.OtherAccountExtension
 }
 
 // AccountGetResult ...
