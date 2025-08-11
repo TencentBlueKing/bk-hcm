312ff62fb946df94809207642837aacd0279dfd4
chen.zhang <422734124@qq.com>
2025-08-11 16:04:18 +0800

    fix:  安全组-防火墙规则点击字段排序存在异常报错且搜索功能存在同样报错  (#1678)
    * fix: VPC将管控区域从搜索条件中去除
    --bug=142474386
    
    * fix: 回收站排序功能失效
    --bug=142317590
    
    * fix: 回收站搜索功能失效
    --bug=142315873
    
    * fix: 镜像-搜索不存的云账号ID可搜索到数据
    --bug=142265464
    
    * fix: 安全组GCP防火墙搜索规则中去除云厂商
    --bug=142170949
    
    * fix: 安全组-防火墙规则点击字段排序存在异常报错且搜索功能存在同样报错
    --bug=142568335
    
    * fix: 用户列表-搜索ID功能优化 --bug=142108803
    
    * fix: 搜索主机/VPC-管控区域搜索功能无效 --bug=142121653

--- front/src/views/resource/resource-manage/children/manage/security-manage.vue
+++ front/src/views/resource/resource-manage/children/manage/security-manage.vue
@@ -233,7 +233,9 @@ const selectSearchData = computed(() => {
         },
       },
     },
-    ...searchData.value,
+    ...searchData.value.filter(
+      (item) => (item.id !== 'vendor' && activeType.value === 'gcp') || activeType.value !== 'gcp',
+    ),
   ];
 
   return [...baseSearchData, ...map[activeType.value].searchData];
@@ -732,7 +734,6 @@ const gcpColumns = [
   {
     label: t('云厂商'),
     field: 'vendor',
-    sort: true,
     isDefaultShow: true,
     render() {
       return h('span', {}, [t('谷歌云')]);
@@ -775,7 +776,6 @@ const gcpColumns = [
   {
     label: t('协议/端口'),
     field: 'allowed_denied',
-    sort: true,
     isDefaultShow: true,
     render({ data }: any) {
       return h(
