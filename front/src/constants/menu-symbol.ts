ea11c3e53a3649f4ac06ec874b586dc4c871bf4e
rickysakura <ricky19991018@126.com>
2025-08-22 10:59:27 +0800

    fix: 移除不需要的枚举 # Reviewed, transaction id: 55235

--- front/src/constants/menu-symbol.ts
+++ front/src/constants/menu-symbol.ts
@@ -39,32 +39,6 @@ export const MENU_SERVICE_HOST_RECYCLE = 'menu_service_host_recycle';
 export const MENU_PLATFORM_MANAGEMENT = 'menu_platform_management';
 
 /**
- * 滚服管理
- */
-export const MENU_ROLLING_SERVER_MANAGEMENT = 'menu_rolling_server_management';
-export const MENU_ROLLING_SERVER_QUOTA = 'menu_rolling_server_quota';
-export const MENU_ROLLING_SERVER_QUOTA_GLOBAL = 'menu_rolling_server_quota_global';
-export const MENU_ROLLING_SERVER_QUOTA_BIZ = 'menu_rolling_server_quota_biz';
-export const MENU_ROLLING_SERVER_USAGE = 'menu_rolling_server_usage';
-export const MENU_ROLLING_SERVER_USAGE_APPLIED = 'menu_rolling_server_usage_applied';
-export const MENU_ROLLING_SERVER_USAGE_BIZ = 'menu_rolling_server_usage_biz';
-
-/**
- * 滚服管理-业务
- */
-export const MENU_BUSINESS_ROLLING_SERVER = 'menu_business_rolling_server';
-
-/**
- * 小额绿通
- */
-export const MENU_GREEN_CHANNEL_MANAGEMENT = 'menu_green_channel_management';
-export const MENU_GREEN_CHANNEL_QUOTA = 'menu_green_channel_quota';
-export const MENU_GREEN_CHANNEL_QUOTA_GLOBAL = 'menu_green_channel_quota_global';
-export const MENU_GREEN_CHANNEL_QUOTA_BIZ = 'menu_green_channel_quota_biz';
-export const MENU_GREEN_CHANNEL_STATS = 'menu_green_channel_stats';
-export const MENU_GREEN_CHANNEL_STATS_BIZ = 'menu_green_channel_stats_biz';
-
-/**
  * 资源接入
  */
 export const MENU_RESOURCE = 'menu_resource';
