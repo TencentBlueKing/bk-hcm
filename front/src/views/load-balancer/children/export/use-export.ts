6990297d34f56bdc89ddb6c0378ad61aadab0edc
chen.zhang <422734124@qq.com>
2025-09-11 18:19:14 +0800

    feat: 负载均衡批量导出优化 --story=126660219

--- front/src/views/load-balancer/children/export/use-export.ts
+++ front/src/views/load-balancer/children/export/use-export.ts
@@ -5,19 +5,42 @@ import { VendorEnum } from '@/common/constant';
 import classes from './export-infobox.module.scss';
 
 type UseExportParams = {
-  target: 'lb' | 'listener';
+  target: 'lb' | 'listener' | 'rs';
   vendor: VendorEnum;
   listeners: Array<{ lb_id: string; lbl_ids?: string[] }>;
   single?: { name: string };
+  only_export_listener?: boolean; // 是否只导出监听器信息
+  check?: boolean; // 是否需要预检
+  target_ids?: string[]; // target为Rs时，此项必填
+};
+
+const targetName: { lb: string; listener: string; rs: string } = {
+  lb: '负载均衡',
+  listener: '监听器',
+  rs: 'Rs',
 };
 
 export const useExport = (params: UseExportParams) => {
-  const { vendor, target = 'lb', listeners = [], single } = params;
+  const {
+    vendor,
+    target = 'lb',
+    listeners = [],
+    target_ids = [],
+    single,
+    only_export_listener = false,
+    check = true,
+  } = params;
 
   const isLb = target === 'lb';
+  const isRs = target === 'rs';
 
   const lbIds = listeners.map((item) => item.lb_id);
   const lblIds = listeners.flatMap((item) => item.lbl_ids);
+  const checkedLength = () => {
+    if (isLb) return lbIds.length;
+    if (isRs) return target_ids.length;
+    return lblIds.length;
+  };
 
   const { exportPreCheck, exportClb } = useLoadBalancerStore();
 
@@ -26,22 +49,23 @@ export const useExport = (params: UseExportParams) => {
       h(
         'div',
         { class: classes['infobox-stats'] },
-        single
-          ? ['名称：', single.name]
-          : ['已选择', h('em', isLb ? lbIds.length : lblIds.length), isLb ? '个负载均衡' : '个监听器'],
+        single ? ['名称：', single.name] : ['已选择', h('em', checkedLength()), `个${targetName[target]}`],
       );
 
     const confirmTips = () =>
-      h('div', { class: classes['infobox-tips'] }, [
-        '导出数据包括',
-        h('b', '监听器信息，URL规则信息(HTTP/HTTPS协议)，监听器绑定的RS信息。'),
-      ]);
+      h(
+        'div',
+        { class: classes['infobox-tips'] },
+        only_export_listener || isRs
+          ? ['导出时间可能较长，点击导出按钮后开始导出']
+          : ['导出数据包括', h('b', '监听器信息，URL规则信息(HTTP/HTTPS协议)，监听器绑定的RS信息。')],
+      );
 
     const errorTips = (props?: { content: string }) => h('div', { class: classes['infobox-tips'] }, props.content);
     const loadingTips = () => h('div', '导出过程中请勿关闭本弹窗，或可直接终止导出');
 
     const infoBox = InfoBox({
-      title: isLb ? `确认${!single ? '批量' : ''}导出负载均衡？` : `确认${!single ? '批量' : ''}导出监听器？`,
+      title: `确认${!single ? '批量' : ''}导出${targetName[target]}？`,
       width: 480,
       contentAlign: 'left',
       content: h('div', { class: classes['infobox-content'] }, [stats(), confirmTips()]),
@@ -53,7 +77,7 @@ export const useExport = (params: UseExportParams) => {
           infoBox.update({
             cancelText: undefined,
           });
-          const checkResult = await exportPreCheck(vendor, listeners);
+          const checkResult = check ? await exportPreCheck(vendor, listeners) : { pass: true, reason: '' };
 
           if (!checkResult.pass) {
             // 预检不通过，显示错误信息
@@ -70,12 +94,17 @@ export const useExport = (params: UseExportParams) => {
             });
           } else {
             // 预检通过，开始导出
-            const { download, cancelDownload } = await exportClb(vendor, listeners);
+            const { download, cancelDownload } = await exportClb(
+              vendor,
+              isRs ? target_ids : listeners,
+              only_export_listener,
+              isRs,
+            );
 
             // 先显示loading
             infoBox.update({
               type: 'loading',
-              title: isLb ? '批量导出负载均衡中…' : '批量导出监听器中…',
+              title: `批量导出${targetName[target]}中…`,
               contentAlign: 'center',
               content: h('div', { class: classes['infobox-content'] }, [loadingTips()]),
               confirmText: undefined,
