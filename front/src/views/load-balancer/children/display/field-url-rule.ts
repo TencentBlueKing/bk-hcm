a10c66296fddacde445607a2048ffa6a22dceff4
chen.zhang <422734124@qq.com>
2025-09-18 10:10:24 +0800

    perf: URL规则列表-展示信息不全 --story=127515455

--- front/src/views/load-balancer/children/display/field-url-rule.ts
+++ front/src/views/load-balancer/children/display/field-url-rule.ts
@@ -2,11 +2,11 @@ import { Column, Model } from '@/decorator';
 
 @Model('load-balancer/rule-display')
 export class DisplayFieldUrlRule {
-  @Column('string', { name: 'VIP 域名' })
-  ip: string[];
+  @Column('array', { name: 'VIP 域名' })
+  lb_vips: string[];
 
   @Column('string', { name: '监听器协议' })
-  lbl_protocols: string;
+  lbl_protocol: string;
 
   @Column('number', { name: '监听器端口' })
   lbl_port: number;
@@ -14,7 +14,7 @@ export class DisplayFieldUrlRule {
   @Column('string', { name: 'URL' })
   rule_url: string;
 
-  @Column('string', { name: '监听器域名' })
+  @Column('array', { name: '监听器域名' })
   rule_domain: string[];
 
   @Column('number', { name: 'RS数量' })
