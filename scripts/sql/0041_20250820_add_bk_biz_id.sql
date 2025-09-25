a35977806dff8a6c26830ee68720d583e1a1f6e3
ZQHcode <1550050864@qq.com>
2025-09-25 17:18:16 +0800

    docs: v1.8.7-alpha.1 --story=126614307

--- scripts/sql/0041_20250820_add_bk_biz_id.sql
+++ scripts/sql/0041_20250820_add_bk_biz_id.sql
@@ -1,5 +1,5 @@
 /*
-    SQLVER=9999,HCMVER=v9.9.9
+    SQLVER=0041,HCMVER=v1.8.7
 
     Notes:
     1. 修改`tcloud_lb_url_rule`表，增加`bk_biz_id`和`account_id`字段
@@ -9,7 +9,7 @@ START TRANSACTION;
 
 ALTER TABLE `tcloud_lb_url_rule`
     ADD COLUMN `bk_biz_id` bigint NOT NULL DEFAULT 0 COMMENT '业务ID' AFTER `cloud_lb_id`,
-    ADD COLUMN `account_id` varchar(64) NOT NULL DEFAULT '' COMMENT '账号ID' AFTER `bk_biz_id`,
+    ADD COLUMN `account_id`  varchar(64) NOT NULL DEFAULT '' COMMENT '账号ID' AFTER `bk_biz_id`,
     ADD INDEX `idx_bk_biz_id` (`bk_biz_id`),
     ADD INDEX `idx_account_id` (`account_id`);
 
@@ -23,6 +23,6 @@ WHERE
     t.bk_biz_id = 0 OR t.account_id = '';
 
 CREATE OR REPLACE VIEW `hcm_version`(`hcm_ver`, `sql_ver`) AS
-SELECT 'v9.9.9' as `hcm_ver`, '9999' as `sql_ver`;
+SELECT 'v1.8.7' as `hcm_ver`, '0041' as `sql_ver`;
 
 COMMIT;
