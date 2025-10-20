3a237e1323919226c4b1695b0ac6960cf7e3c98f
ironguo <783332991@qq.com>
2025-10-20 11:21:27 +0800

    feat: 查询监听器-增加VIP数量的限制 --story=128018438

--- pkg/api/data-service/cloud/load_balancer.go
+++ pkg/api/data-service/cloud/load_balancer.go
@@ -642,7 +642,7 @@ type ListListenerByCondReq struct {
 	BkBizID           int64               `json:"bk_biz_id" validate:"omitempty"`
 	Vendor            enumor.Vendor       `json:"vendor" validate:"required,min=1"`
 	AccountID         string              `json:"account_id" validate:"required,min=1"`
-	ListenerQueryList []ListenerQueryLine `json:"rule_query_list" validate:"required,min=1,max=50"`
+	ListenerQueryList []ListenerQueryLine `json:"rule_query_list" validate:"required,min=1"`
 }
 
 // Validate request.
@@ -655,10 +655,18 @@ func (req *ListListenerByCondReq) Validate() error {
 		return errors.New("rule_query_list is empty")
 	}
 
+	totalCLBIDs := 0
+	totalCLBVipDomains := 0
 	for _, item := range req.ListenerQueryList {
 		if err := item.Validate(); err != nil {
 			return err
 		}
+		totalCLBIDs += len(item.CloudLbIDs)
+		totalCLBVipDomains += len(item.ClbVipDomains)
+	}
+	// 和平等业务的CLB较多，上限暂定为3000
+	if totalCLBIDs > 3000 || totalCLBVipDomains > 3000 {
+		return errors.New("cloud_lb_ids and clb_vip_domains num must be less than 3000")
 	}
 
 	return validator.Validate.Struct(req)
@@ -668,8 +676,8 @@ func (req *ListListenerByCondReq) Validate() error {
 type ListenerQueryLine struct {
 	Protocol      enumor.ProtocolType `json:"protocol" validate:"required,min=1"`
 	Region        string              `json:"region" validate:"required,min=1"`
-	ClbVipDomains []string            `json:"clb_vip_domains" validate:"required,min=1,max=50"`
-	CloudLbIDs    []string            `json:"cloud_lb_ids" validate:"required,min=1,max=50"`
+	ClbVipDomains []string            `json:"clb_vip_domains" validate:"required,min=1,max=3000"`
+	CloudLbIDs    []string            `json:"cloud_lb_ids" validate:"required,min=1,max=3000"`
 	Ports         []int64             `json:"ports" validate:"omitempty,max=500"`
 	RsIPs         []string            `json:"rs_ips" validate:"omitempty,max=500"`
 	RsPorts       []int64             `json:"rs_ports" validate:"omitempty,max=500"`
@@ -683,11 +691,6 @@ func (req *ListenerQueryLine) Validate() error {
 
 	if len(req.ClbVipDomains) != len(req.CloudLbIDs) {
 		return errors.New("clb_vip_domains and cloud_lb_ids num must be equal")
-	}
-
-	// 传入的负载均衡ID数量不能超过50个
-	if len(req.CloudLbIDs) > 50 {
-		return errors.New("cloud_lb_ids num must be less than 50")
 	}
 
 	// 传入的RSIP数量不能超过500个
