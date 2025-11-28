a8c1c548b50a0ae7817b62e33a72acfe6cfb88c8
chen.zhang <422734124@qq.com>
2025-11-28 11:18:33 +0800

    perf: CLB-页面支持端口段 --story=128701575

--- front/src/views/load-balancer/listener/add.vue
+++ front/src/views/load-balancer/listener/add.vue
@@ -55,9 +55,17 @@ const rules = {
       trigger: 'change',
     },
   ],
-  port: [
+  port_segment: [
     {
-      validator: (value: number) => value >= 1 && value <= 65535,
+      validator: (value: string) => {
+        const val = value.split('-');
+        const [port, end_port] = val;
+        if (val.length > 2) return false; // eq: xx-xx-xx
+        if (val.length === 2 && !end_port) return false; // eq xx-
+        const portVali = +port >= 1 && +port <= 65535;
+        const endPortVali = end_port ? +end_port >= 1 && +end_port <= 65535 : true;
+        return portVali && endPortVali;
+      },
       message: '端口号不符合规范',
       trigger: 'change',
     },
@@ -93,13 +101,15 @@ const rules = {
     },
   ],
 };
-const formModel = reactive<IListenerModel>({
+const formModel = reactive<IListenerModel & { port_segment: string }>({
   id: '',
   account_id: props.loadBalancerDetails.account_id,
   lb_id: props.loadBalancerDetails.id,
   name: '',
   protocol: ListenerProtocol.TCP,
   port: undefined,
+  end_port: undefined,
+  port_segment: undefined,
   scheduler: undefined,
   session_open: false,
   session_type: SessionType.NORMAL,
@@ -125,6 +135,9 @@ watchEffect(() => {
       ca_cloud_id: '',
       cert_cloud_ids: [],
     },
+    port_segment: props.initialModel.end_port
+      ? `${props.initialModel.port}-${props.initialModel.end_port}`
+      : props.initialModel.port,
   });
   bindingTargetGroupName.value = target_group_name;
   isSniOpen.value = !!sni_switch;
@@ -249,9 +262,13 @@ const handleConfirm = async () => {
     }
     Message({ theme: 'success', message: '更新成功' });
   } else {
+    // 将port_segment分解成port和end_port
+    const [port, end_port] = formModel.port_segment.split('-');
+    formModel.port = +port;
+    formModel.end_port = +end_port;
     await loadBalancerListenerStore.addListener(
       {
-        ...formModel,
+        ...{ ...formModel, port_segment: undefined },
         id: undefined,
         session_open: undefined,
         // session_type: isLayer4 && Scheduler.LEAST_CONN !== scheduler ? session_type : undefined, // 后端要求必填
@@ -305,8 +322,8 @@ const { beforeClose } = useSideslider(formModel);
           </bk-radio-button>
         </bk-radio-group>
       </bk-form-item>
-      <bk-form-item label="监听端口" required property="port">
-        <bk-input v-model.number="formModel.port" type="number" :disabled="isEdit" />
+      <bk-form-item label="监听端口" required property="port_segment">
+        <bk-input v-model="formModel.port_segment" :disabled="isEdit" />
       </bk-form-item>
       <template v-if="ListenerProtocol.HTTPS === formModel.protocol">
         <div class="flex-row justify-content-between">
