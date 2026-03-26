4b1fcb82efe67eb2fe867675b6d93cfb52bd2d99
chen.zhang <422734124@qq.com>
2026-03-26 18:26:50 +0800

    chore: 注释二级账号入口部分代码

--- front/src/views/cloud-account-manage/permission-policy/children/data-list/data-list.vue
+++ front/src/views/cloud-account-manage/permission-policy/children/data-list/data-list.vue
@@ -37,9 +37,9 @@ const handleViewDetails = (row: IPermissionPolicyItem) => {
 };
 
 // 应用到二级账号
-const handleApplyToAccount = (row: IPermissionPolicyItem) => {
-  emit('apply-to-account', row);
-};
+// const handleApplyToAccount = (row: IPermissionPolicyItem) => {
+//   emit('apply-to-account', row);
+// };
 
 // 编辑
 const handleEditAccount = (row: IPermissionPolicyItem) => {
@@ -140,10 +140,10 @@ const isRelatedAccountColumn = (column: ModelPropertyColumn) => column.id === 'r
           </template>
         </template>
       </bk-table-column>
-      <bk-table-column label="操作" width="180" fixed="right">
+      <bk-table-column label="操作" width="80" fixed="right" v-if="!isBusinessPage">
         <template #default="{ row }">
-          <bk-button theme="primary" text @click="handleEditAccount(row)" v-if="!isBusinessPage">编辑</bk-button>
-          <bk-button theme="primary" text @click="handleApplyToAccount(row)">应用到二级账号</bk-button>
+          <bk-button theme="primary" text @click="handleEditAccount(row)">编辑</bk-button>
+          <!-- <bk-button theme="primary" text @click="handleApplyToAccount(row)">应用到二级账号</bk-button> -->
         </template>
       </bk-table-column>
     </bk-table>
