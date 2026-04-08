6baf16c6d6e3a90df279c06cbb6bc4725c550f51
Xavier-wa <756258495@qq.com>
2026-04-08 16:22:28 +0800

    feat: 代码review优化  --story=130359800

--- cmd/cloud-server/service/sub-account/list.go
+++ cmd/cloud-server/service/sub-account/list.go
@@ -299,9 +299,7 @@ func (svc *service) listBizSubAccountAuthRes(cts *rest.Contexts, reqFilter *filt
 		return scopeFilter, false, nil
 	}
 
-	// 过滤主账号
-	finalFilter, err := tools.And(scopeFilter, reqFilter,
-		tools.RuleNotEqual("account_type", string(enumor.MainAccount)))
+	finalFilter, err := tools.And(scopeFilter, reqFilter)
 	if err != nil {
 		return nil, false, err
 	}
